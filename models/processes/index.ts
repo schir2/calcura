bd8ce949abef8ffea1b5b8d703bf3dd72dab0de2
Marek Schir <schir2@msn.com>
2024-11-11 19:01:25 -0500

    removed elective limit process

--- models/processes/index.ts
+++
@@ -1,7 +0,0 @@
-import ElectiveLimitProcess from './electiveLimitsProcess'
-
-
-// Initialize and export the pipeline instances directly
-export const process = {
-    electiveLimit: new ElectiveLimitProcess(),
-};
