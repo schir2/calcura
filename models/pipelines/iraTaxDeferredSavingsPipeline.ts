cb3a14ef7a39aadf9035853a6047d681c4fc5811
Marek Schir <schir2@gmail.com>
2024-11-12 09:01:10 -0500

    reworked pipelines into functional pipelines

--- models/pipelines/iraTaxDeferredSavingsPipeline.ts
+++ models/pipelines/iraTaxDeferredSavingsPipeline.ts
@@ -1,17 +1,16 @@
-import type {InvestmentPipeline} from "~/interfaces/InvestmentPipeline";
 import type Row from "~/models/Row";
 
 
 export function iraTaxDeferredSavingsPipeline(row: Row): Row {
-        assertDefined(row.savingsEndOfYear, 'savingsEndOfYear')
-        assertDefined(row.incomeTaxable, 'incomeTaxable')
-        row.savingsStartOfYear = row.savingsEndOfYear
-        row.iraTaxDeferredContribution = row.calculateIraDeferredContribution()
-        row.iraTaxDeferredGrowthAmount = row.calculateIraTaxDeferredGrowthAmount()
-        row.iraTaxDeferredSavingsEndOfYear = row.calculateIraTaxDeferredSavingsEndOfYear()
-        row.iraTaxDeferredContributionLifetime += row.iraTaxDeferredContribution
-        row.savingsEndOfYear += row.iraTaxDeferredContribution + row.iraTaxDeferredGrowthAmount
-        row.taxDeferredSpending += row.iraTaxDeferredContribution
-        row.incomeTaxable -= row.iraTaxDeferredContribution
-        return row
-    }
+    assertDefined(row.savingsEndOfYear, 'savingsEndOfYear')
+    assertDefined(row.incomeTaxable, 'incomeTaxable')
+    row.savingsStartOfYear = row.savingsEndOfYear
+    row.iraTaxDeferredContribution = row.calculateIraDeferredContribution()
+    row.iraTaxDeferredGrowthAmount = row.calculateIraTaxDeferredGrowthAmount()
+    row.iraTaxDeferredSavingsEndOfYear = row.calculateIraTaxDeferredSavingsEndOfYear()
+    row.iraTaxDeferredContributionLifetime += row.iraTaxDeferredContribution
+    row.savingsEndOfYear += row.iraTaxDeferredContribution + row.iraTaxDeferredGrowthAmount
+    row.taxDeferredSpending += row.iraTaxDeferredContribution
+    row.incomeTaxable -= row.iraTaxDeferredContribution
+    return row
+}
