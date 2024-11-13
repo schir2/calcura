9935275ffc3d8d2c5a8e4338733bed3d40f3ec48
Marek Schir <schir2@msn.com>
2024-11-13 01:35:07 -0500

    added pinia and basic body classes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     css: [
         '@/assets/css/tailwind.css'
     ],
     devtools: {enabled: true},
-    modules: ['@nuxt/test-utils/module', 'nuxt-feather-icons'],
+    modules: ['@nuxt/test-utils/module', 'nuxt-feather-icons', '@pinia/nuxt',],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
+    app: {
+        head: {
+            bodyAttrs: {
+                class: 'bg-skin-primary text-lg text-skin-base'
+            }
+        }
+    }
 })
