f7efd194d1f99d21c58964a3bba5543d1e87de56
Marek Schir <schir2@msn.com>
2024-11-13 01:35:31 -0500

    removed unused interfaces

--- interfaces/Pipeline.ts
+++
@@ -1,7 +0,0 @@
-import type Row from "~/models/Row";
-
-export interface Pipeline {
-
-    process(row: Row): Row;
-
-}
--- interfaces/Process.ts
+++
@@ -1,7 +0,0 @@
-import type Row from "~/models/Row";
-
-export interface Process {
-
-    process(row: Row): Row;
-
-}
