da73da753d0429d802e52fb5ba847e41861c918f
Marek Schir <schir2@msn.com>
2024-11-13 01:34:26 -0500

    converted end of year pipeline into a function

--- models/pipelines/endOfYearPipeline.ts
+++ models/pipelines/endOfYearPipeline.ts
@@ -1,25 +1,21 @@
 import type Row from "~/models/Row";
-import type {Pipeline} from "~/interfaces/Pipeline";
 
-export default class EndOfYearPipeline implements Pipeline {
+export function endOfYearPipeline(row: Row): Row {
+    row.incomeTaxable = row.calculateIncomeTaxable()
+    row.incomeTaxAmount = row.calculateIncomeTaxAmount(undefined)
+    row.incomeTaxed = row.calculateIncomeTaxed()
+    row.incomeDisposable = row.calculateIncomeDisposable()
+    row.cashEndOfYear = row.calculateCashEndOfYear()
 
-    process(row: Row): Row {
-        row.incomeTaxable = row.calculateIncomeTaxable()
-        row.incomeTaxAmount = row.calculateIncomeTaxAmount(undefined)
-        row.incomeTaxed = row.calculateIncomeTaxed()
-        row.incomeDisposable = row.calculateIncomeDisposable()
-        row.cashEndOfYear = row.calculateCashEndOfYear()
+    row.debts.forEach((debt) => {
+        const {principalEndOfYear, interestAmount, interestAccrued, paymentLifetime} = debt.calculateEndOfYearValues();
 
-        row.debts.forEach((debt) => {
-            const {principalEndOfYear, interestAmount, interestAccrued, paymentLifetime} = debt.calculateEndOfYearValues();
-
-            debt.principalEndOfYear = principalEndOfYear;
-            debt.interestAmount = interestAmount;
-            debt.interestAccrued = interestAccrued;
-            debt.paymentLifetime = paymentLifetime;
-        })
-        row.debtEndOfYear = row.calculateTotalDebtEndOfYear()
-        row.retirementIncomeProjected = row.calculateRetirementIncomeProjected()
-        return row
-    }
+        debt.principalEndOfYear = principalEndOfYear;
+        debt.interestAmount = interestAmount;
+        debt.interestAccrued = interestAccrued;
+        debt.paymentLifetime = paymentLifetime;
+    })
+    row.debtEndOfYear = row.calculateTotalDebtEndOfYear()
+    row.retirementIncomeProjected = row.calculateRetirementIncomeProjected()
+    return row
 }
