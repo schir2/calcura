98c10e68e6f7ce3ab178b4f4cb6b927aa25e4f14
Marek Schir <schir2@msn.com>
2024-11-13 01:33:59 -0500

    added initial layout for main

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,14 @@
     <!-- Navbar -->
     <LayoutTheNavbar/>
 
-    <div class="layout-body">
+    <div>
       <!-- Sidebar -->
       <LayoutTheSidebar/>
 
       <!-- Main Content -->
-      <main class="layout-content">
+      <main class="flex-grow mx-auto container p-16 bg-skin-surface shadow-md rounded-md space-y-12">
         <slot/>
       </main>
     </div>
-
-    <!-- Footer (optional) -->
-    <LayoutTheFooter/>
   </div>
 </template>
