e4f8768613631eabb604c8ab88e92aafd175f0cd
Marek Schir <schir2@gmail.com>
2024-11-14 13:28:20 -0500

    added basic card component

--- components/common/Card.vue
+++ components/common/Card.vue
@@ -3,6 +3,15 @@
 </script>
 
 <template>
+  <div class="p-6 rounded-lg shadow-sm bg-skin-primary hover:bg-skin-secondary">
+    <div v-if="$slots.header">
+      <slot name="header"></slot>
+    </div>
+    <slot/>
+    <div v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
 
 </template>
 
