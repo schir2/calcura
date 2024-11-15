4c28940df85fa8b6ee7468c029b9ebeebc914d28
Marek Schir <schir2@msn.com>
2024-11-15 11:07:27 -0500

    added card layout for info

--- components/retirement/Profile.vue
+++ components/retirement/Profile.vue
@@ -7,12 +7,14 @@ const profile = ref({
 </script>
 
 <template>
-  <form>
-    <h2 class="text-2xl">Profile</h2>
-    <FormInput name="name" type="text" label="Name" v-model="profile.name" />
-    <FormInput name="age" type="number" label="Age" v-model="profile.age" :rules="{min_value:0, max_value:120, required:true}" />
-    <FormInput name="lifeExpectancy" type="number" label="Life Expectancy" :rules="{min_value:profile.age, max_value:120, required:true}" v-model="profile.lifeExpectancy"/>
-  </form>
+  <CommonCard>
+    <form>
+      <h2 class="text-2xl">Profile</h2>
+      <FormInput name="name" type="text" label="Name" v-model="profile.name"/>
+      <FormInput name="age" type="number" label="Age" v-model="profile.age" :rules="{min_value:0, max_value:120, required:true}"/>
+      <FormInput name="lifeExpectancy" type="number" label="Life Expectancy" :rules="{min_value:profile.age, max_value:120, required:true}" v-model="profile.lifeExpectancy"/>
+    </form>
+  </CommonCard>
 </template>
 
 <style scoped>
