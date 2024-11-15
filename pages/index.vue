7d68bd3c058c78a033f5ccc8ab0cb41b4b8a6011
Marek Schir <schir2@msn.com>
2024-11-15 11:07:05 -0500

    added grid layout and summary

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <RetirementProfile/>
-  <RetirementTaxDeferredInvestment/>
+  <div class="grid grid-cols-3">
+    <LayoutTheSidebar/>
+    <div class="space-y-6">
+      <RetirementProfile/>
+      <RetirementTaxDeferredInvestment/>
+    </div>
+    <RetirementSummary/>
+  </div>
 </template>
 <script setup lang="ts">
 useHead({
