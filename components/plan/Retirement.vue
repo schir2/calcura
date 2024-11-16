b9ba6d4a3e59e8af2f2bde49dbefa243b555d952
Marek Schir <schir2@gmail.com>
2024-11-16 17:30:07 -0500

    updated retirement component

--- components/plan/Retirement.vue
+++ components/plan/Retirement.vue
@@ -4,22 +4,25 @@ import {retirementFields} from "~/forms/retirementForm";
 import type {RetirementData} from "~/models/Retirement";
 
 const form = reactive(new FormModel<RetirementData>(retirementFields));
-
-
+interface Props{
+  showAdvancedOptions?: boolean;
+}
+const {showAdvancedOptions = false} = defineProps<Props>()
 </script>
 <template>
   <CommonCard>
-    <h2 class="text-2xl">Plan: {{ form.retirementStrategy.value }}</h2>
-    <Form>
+    <h2 class="text-2xl">Retirement Plan</h2>
+    <Form class="grid grid-cols-6 gap-3">
       <FormField :field="form.age"/>
-      <FormField :field="form.year"/>
-      <FormField :field="form.lifeExpectancy"/>
-      <FormField :field="form.retirementStrategy"/>
+      <FormField v-show="showAdvancedOptions" :field="form.year"/>
+      <FormField v-show="showAdvancedOptions" :field="form.lifeExpectancy"/>
+      <FormSelect :field="form.retirementStrategy"/>
       <FormField :field="form.retirementWithdrawalRate"/>
-      <FormField v-if="form.retirementStrategy.value === 'targetSavings'" :field="form.retirementIncomeGoal"/>
-      <FormField :field="form.retirementAge"/>
-      <FormField :field="form.retirementSavingsAmount"/>
-      <FormField :field="form.retirementIncomeProjected"/>
+      <FormField v-if="form.retirementStrategy.value === 'age'" :field="form.retirementAge"/>
+
+      <FormField v-if="form.retirementStrategy.value === 'targetSavings'" :field="form.retirementSavingsAmount"/>
+      <FormField v-if="form.retirementStrategy.value === 'percentRule'" :field="form.retirementIncomeGoal"/>
+      <FormField v-if="form.retirementStrategy.value === 'percentRule'" :field="form.retirementIncomeProjected"/>
     </Form>
   </CommonCard>
 </template>
