ace80757f769aaa3e55cd6baf4c42978b6baadd6
Marek Schir <schir2@gmail.com>
2024-11-16 17:27:05 -0500

    added income constants

---
+++ constants/income.ts
@@ -0,0 +1,16 @@
+export const DEFAULT_INCOME_NAME = '';
+export const DEFAULT_GROSS_INCOME = 37585;
+export const DEFAULT_GROWTH_RATE = 1.4;
+export const DEFAULT_TAX_RATE = 30;
+
+export const DEFAULT_GROWTH_STRATEGY = 'percentage';
+
+export const DEFAULT_TAX_STRATEGY = 'simple';
+
+export const MIN_INCOME_NAME_LENGTH = 3;
+export const MAX_INCOME_NAME_LENGTH = 32;
+export const MIN_GROSS_INCOME = 0;
+export const MIN_GROWTH_RATE = 0;
+export const MAX_GROWTH_RATE = 100;
+export const MIN_TAX_RATE = 0;
+export const MAX_TAX_RATE = 100;
