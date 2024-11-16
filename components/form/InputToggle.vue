a2358ff4f79a457fe85ac81a9299b95fbdc73e64
Marek Schir <schir2@gmail.com>
2024-11-16 17:28:45 -0500

    reorganized file

--- components/form/InputToggle.vue
+++ components/form/InputToggle.vue
@@ -46,8 +46,7 @@ const onInput = (event: Event) => {
             :rules="rules"
             @input="onInput"
             class="sr-only peer"/>
-        <div
-            class="relative w-11 h-6 bg-skin-primary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-skin-accent rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-skin-primary after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-skin-base after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-skin-accent"></div>
+        <div class="relative w-11 h-6 bg-skin-primary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-skin-accent rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-skin-primary after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-skin-base after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-skin-accent"></div>
         <span class="ms-3 text-sm font-medium text-skin-base">{{ helpText }}</span>
       </label>
     </div>
