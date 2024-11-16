b8b569045dde255ac84c47eaf254efb1695784ba
Marek Schir <schir2@gmail.com>
2024-11-16 17:27:50 -0500

    added income

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="col-span-4 space-y-6">
-    <PlanRetirement/>
-<!--        <PlanTaxDeferredInvestment/>-->
+    <InputToggle name="showAdvancedOptions" v-model="showAdvancedOptions" label="Show Advanced Options" />
+    <PlanRetirement :showAdvancedOptions="showAdvancedOptions"/>
+    <PlanIncome :showAdvancedOptions="showAdvancedOptions"></PlanIncome>
+    <PlanTaxDeferredInvestment :showAdvancedOptions="showAdvancedOptions"/>
   </div>
   <PlanSummary class="col-span-1"/>
 </template>
 <script setup lang="ts">
+import InputToggle from "~/components/form/InputToggle.vue";
+
 useHead({
   title: 'Calcura Dashboard',
   meta: [
     {name: 'description', content: 'Calcura: Dashboard'}
   ],
 })
+const showAdvancedOptions = ref<boolean>(false)
 </script>
