e9045a6a3724ead1a0fa16e76a4ec91ac5216a78
Marek Schir <schir2@gmail.com>
2024-11-16 17:27:16 -0500

    added initial income component

---
+++ components/plan/Income.vue
@@ -0,0 +1,30 @@
+<template>
+  <CommonCard>
+    <h2 class="text-2xl">Income</h2>
+    <Form>
+      <section>
+        <div class="grid grid-cols-6 gap-3">
+            <FormField :field="form.name"></FormField>
+            <FormField :field="form.grossIncome"></FormField>
+            <FormSelect :field="form.growthStrategy"></FormSelect>
+            <FormField :field="form.growthRate"></FormField>
+            <FormField :field="form.taxRate"></FormField>
+            <FormSelect :field="form.taxStrategy"></FormSelect>
+        </div>
+      </section>
+    </Form>
+  </CommonCard>
+
+</template>
+<script setup lang="ts">
+import type {IncomeData} from "~/models/Income";
+import {incomeFields} from "~/forms/incomeForm";
+
+interface Props {
+  showAdvancedOptions: boolean;
+}
+
+const form = reactive(new FormModel<IncomeData>(incomeFields))
+
+const {showAdvancedOptions = false} = defineProps<Props>()
+</script>
