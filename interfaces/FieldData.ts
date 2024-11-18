5f4e874ca0fc3163d694dd74a1adffb1db911af7
Marek Schir <schir2@gmail.com>
2024-11-17 21:25:59 -0500

    removed value and key from field data

--- interfaces/FieldData.ts
+++ interfaces/FieldData.ts
@@ -2,9 +2,7 @@ import * as yup from "yup";
 import type {SelectOption} from "~/components/form/BaseSelect.vue";
 
 export interface FieldData<T = any> {
-    key: keyof T;
     name: string;
-    value?: T[keyof T];
     label: string;
     placeholder?: string;
     helpText?: string;
