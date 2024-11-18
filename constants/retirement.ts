1df3ba8d60d44692ce18374825765851bc4092e8
Marek Schir <schir2@gmail.com>
2024-11-17 21:28:37 -0500

    added new constants for retirement including templates

--- constants/retirement.ts
+++ constants/retirement.ts
@@ -1,3 +1,7 @@
+import type {RetirementData} from "~/models/Retirement";
+
+export const DEFAULT_RETIREMENT_PLAN_NAME: string = 'Retirement Plan'
+
 export const DEFAULT_RETIREMENT_AGE: number = 30;
 export const MIN_RETIREMENT_AGE: number = 1;
 export const MAX_RETIREMENT_AGE: number = 120;
@@ -23,3 +27,17 @@ export const MAX_RETIREMENT_SAVINGS_AMOUNT = 10000000;
 
 export const MIN_RETIREMENT_AGE_FOR_WITHDRAWAL = 30;
 export const MAX_RETIREMENT_AGE_FOR_WITHDRAWAL = 100;
+
+export const RETIREMENT_TEMPLATE: Record<string, RetirementData> = {
+    default: {
+        name: 'Retire by a Certain Age',
+        retirementStrategy: 'age',
+        year: DEFAULT_RETIREMENT_YEAR,
+        age: DEFAULT_RETIREMENT_AGE,
+        retirementAge: DEFAULT_RETIREMENT_AGE,
+        lifeExpectancy: DEFAULT_RETIREMENT_LIFE_EXPECTANCY,
+        retirementSavingsAmount: 0,
+        retirementWithdrawalRate: 0,
+        retirementIncomeGoal: 0,
+    }
+}
