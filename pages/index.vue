fbc4f79171375ff0013deafe3107bbb3cc7b559c
Marek Schir <schir2@gmail.com>
2024-11-17 21:27:45 -0500

    added add income buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="col-span-4 space-y-6">
-    <InputToggle name="showAdvancedOptions" v-model="showAdvancedOptions" label="Show Advanced Options" />
+    <InputToggle name="showAdvancedOptions" v-model="showAdvancedOptions" label="Show Advanced Options"/>
     <PlanRetirement :showAdvancedOptions="showAdvancedOptions"/>
-    <PlanIncome :showAdvancedOptions="showAdvancedOptions"></PlanIncome>
+    <h2 class="text-2xl">Income(s)</h2>
+
+    <CommonButton @click="plan.addIncome()">Add Income</CommonButton>
+    <PlanIncome
+        v-for="(income, index) in plan.incomes"
+        :income="income"
+        :key="index"
+        :incomeIndex="index"
+        :showAdvancedOptions="showAdvancedOptions"
+        @deleteIncome="handleDeleteIncome"
+    ></PlanIncome>
     <PlanTaxDeferredInvestment :showAdvancedOptions="showAdvancedOptions"/>
   </div>
   <PlanSummary class="col-span-1"/>
 </template>
 <script setup lang="ts">
 import InputToggle from "~/components/form/InputToggle.vue";
+import Plan from "~/models/Plan";
 
 useHead({
   title: 'Calcura Dashboard',
@@ -16,5 +27,12 @@ useHead({
     {name: 'description', content: 'Calcura: Dashboard'}
   ],
 })
+
+function handleDeleteIncome(payload: { index: number }) {
+  plan.deleteIncome(payload.index)
+}
+
+const plan = reactive(new Plan(Plan.defaultValues()))
+
 const showAdvancedOptions = ref<boolean>(false)
 </script>
