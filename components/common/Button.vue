b7ed04f9403e0a0f0bb2ca811d2f1baee53c6d23
Marek Schir <schir2@gmail.com>
2024-11-17 21:25:20 -0500

    added styling for button

--- components/common/Button.vue
+++ components/common/Button.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+interface Props {
+  disabled: boolean,
+}
+
+const {disabled = false} = defineProps<Props>()
 
 </script>
 
 <template>
-<button><slot/></button>
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <slot name="leftIcon"></slot>
+    <slot></slot>
+    <slot name="rightIcon"></slot>
+  </button>
+
+
 </template>
 
 <style scoped>
