1ff829f32e64ec1dbc5f6c91ebeaf3b43c7d8d5a
Marek Schir <schir2@gmail.com>
2024-11-17 22:43:49 -0500

    added debt

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,42 @@
   <div class="col-span-4 space-y-6">
     <InputToggle name="showAdvancedOptions" v-model="showAdvancedOptions" label="Show Advanced Options"/>
     <PlanRetirement :showAdvancedOptions="showAdvancedOptions"/>
-    <h2 class="text-2xl">Income(s)</h2>
-
-    <CommonButton @click="plan.addIncome()">Add Income</CommonButton>
-    <PlanIncome
-        v-for="(income, index) in plan.incomes"
-        :income="income"
-        :key="index"
-        :incomeIndex="index"
-        :showAdvancedOptions="showAdvancedOptions"
-        @deleteIncome="handleDeleteIncome"
-    ></PlanIncome>
-    <PlanTaxDeferredInvestment :showAdvancedOptions="showAdvancedOptions"/>
+    <section>
+      <h2 class="text-3xl">Income(s)</h2>
+      <CommonButton @click="plan.addIncome()">Add Income</CommonButton>
+      <PlanIncome
+          v-for="(income, index) in plan.incomes"
+          :income="income"
+          :key="index"
+          :incomeIndex="index"
+          :showAdvancedOptions="showAdvancedOptions"
+          @deleteIncome="handleDeleteIncome"
+      ></PlanIncome>
+    </section>
+    <section>
+      <h2 class="text-3xl">Debt(s)</h2>
+      <CommonButton @click="plan.addDebt()">Add Debt</CommonButton>
+      <PlanDebt
+          v-for="(debt, index) in plan.debts"
+          :debt="debt"
+          :key="index"
+          :debtIndex="index"
+          :showAdvancedOptions="showAdvancedOptions"
+          @deleteDebt="handleDeleteDebt"
+      ></PlanDebt>
+    </section>
+    <section>
+      <h2 class="text-3xl">Tax Deferred Investments</h2>
+      <CommonButton @click="plan.addTaxDeferredInvestment()">Add Income</CommonButton>
+      <PlanTaxDeferredInvestment
+          v-for="(investment, index) in plan.taxDeferredInvestments"
+          :investment="investment"
+          :key="index"
+          :investmentIndex="index"
+          :showAdvancedOptions="showAdvancedOptions"
+          @deleteInvestment="handleDeleteTaxDeferredInvestment"
+      />
+    </section>
   </div>
   <PlanSummary class="col-span-1"/>
 </template>
@@ -30,6 +54,14 @@ useHead({
 
 function handleDeleteIncome(payload: { index: number }) {
   plan.deleteIncome(payload.index)
+}
+
+function handleDeleteTaxDeferredInvestment(payload: { index: number }) {
+  plan.deleteTaxDeferredInvestment(payload.index)
+}
+
+function handleDeleteDebt(payload: { index: number }) {
+  plan.deleteDebt(payload.index)
 }
 
 const plan = reactive(new Plan(Plan.defaultValues()))
