a24bb54852c19139c1b317ee1da137a82beb5737
Marek Schir <schir2@gmail.com>
2024-11-18 22:54:46 -0500

    added DEFAULT_RETIRE_AT_AGE constant

--- components/plan/Retirement.vue
+++ components/plan/Retirement.vue
@@ -1,16 +1,16 @@
 <template>
   <CommonCard>
-    <h2 class="text-3xl">Retirement Plan: {{retirement.name}}</h2>
+    <h2 class="text-3xl">Retirement Plan: {{ retirement.name }}</h2>
     <Form class="grid grid-cols-6 gap-3">
       <FormField :model="retirement" :field="fieldMetadata.name"/>
+      <FormSelect :model="retirement" :field="fieldMetadata.retirementStrategy"/>
       <FormField :model="retirement" :field="fieldMetadata.age"/>
       <FormField :model="retirement" v-show="showAdvancedOptions" :field="fieldMetadata.year"/>
       <FormField :model="retirement" v-show="showAdvancedOptions" :field="fieldMetadata.lifeExpectancy"/>
-      <FormSelect :model="retirement" :field="fieldMetadata.retirementStrategy"/>
-      <FormField :model="retirement" :field="fieldMetadata.retirementWithdrawalRate"/>
       <FormField :model="retirement" v-if="retirement.retirementStrategy === 'age'" :field="fieldMetadata.retirementAge"/>
 
       <FormField :model="retirement" v-if="retirement.retirementStrategy === 'targetSavings'" :field="fieldMetadata.retirementSavingsAmount"/>
+      <FormField :model="retirement" v-if="retirement.retirementStrategy === 'percentRule'" :field="fieldMetadata.retirementWithdrawalRate"/>
       <FormField :model="retirement" v-if="retirement.retirementStrategy === 'percentRule'" :field="fieldMetadata.retirementIncomeGoal"/>
     </Form>
   </CommonCard>
