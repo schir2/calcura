cc54faa71e06138fa59502e3d4a29e4dd5574db0
Marek Schir <schir2@gmail.com>
2024-11-18 22:52:47 -0500

    added expense component

---
+++ components/plan/Expense.vue
@@ -0,0 +1,23 @@
+<template>
+  <CommonCard>
+    <FormField :model="expense" :field="fieldMetadata.name"></FormField>
+    <FormField :model="expense" :field="fieldMetadata.amount"></FormField>
+    <FormSelect :model="expense" :field="fieldMetadata.frequency"></FormSelect>
+    <FormSelect :model="expense" :field="fieldMetadata.type"></FormSelect>
+    <FormField :model="expense" :field="fieldMetadata.isEssential"></FormField>
+    <FormField :model="expense" :field="fieldMetadata.isTaxDeductible"></FormField>
+  </CommonCard>
+</template>
+<script setup lang="ts">
+import type Expense from "~/models/Expense";
+import {expenseFields} from "~/forms/expenseForm";
+
+interface Props {
+  expense: Expense,
+  expenseIndex: number,
+}
+
+const {expense, expenseIndex} = defineProps<Props>()
+
+const fieldMetadata = expenseFields
+</script>
