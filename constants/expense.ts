b336fb38419db6e7ec31a6955aa446dbc1d4b9ce
Marek Schir <schir2@gmail.com>
2024-11-18 22:52:38 -0500

    added expense constants

---
+++ constants/expense.ts
@@ -0,0 +1,4 @@
+export const MIN_EXPENSE_AMOUNT = 0;
+export const MAX_EXPENSE_AMOUNT = 1_000_000; // Adjust as needed
+export const EXPENSE_NAME_MIN_LENGTH = 3;
+export const EXPENSE_NAME_MAX_LENGTH = 100;
