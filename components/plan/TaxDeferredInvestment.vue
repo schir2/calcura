5e968a1c950562dfcd39467acda41608d99a29ad
Marek Schir <schir2@gmail.com>
2024-11-18 22:55:04 -0500

    added transitions

--- components/plan/TaxDeferredInvestment.vue
+++ components/plan/TaxDeferredInvestment.vue
@@ -1,39 +1,44 @@
 <template>
-  <CommonCard>
-    <Form class="space-y-6">
-      <div class="flex justify-between align-middle">
-        <h3 class="text-2xl">{{ taxDeferredInvestment.name }}</h3>
-        <CommonButton @click="deleteInvestment(investmentIndex)">
-          <TrashIcon/>
-        </CommonButton>
-      </div>
-      <div class="grid grid-cols-3 gap-3">
-        <FormField :model="taxDeferredInvestment" :field="fieldMetadata.name"/>
-        <FormField :model="taxDeferredInvestment" :field="fieldMetadata.balance"/>
-        <FormField :model="taxDeferredInvestment" v-show="showAdvancedOptions" :field="fieldMetadata.growthRate"/>
-      </div>
-      <section>
-        <h3 class="text-xl">Elective Contributions</h3>
-        <div class="grid grid-cols-3 gap-3">
-          <FormSelect :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionStrategy"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionPercentage"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionFixedAmount"/>
+    <CommonCard>
+      <Form class="space-y-6">
+        <div class="flex justify-between align-middle">
+          <h3 class="text-2xl">{{ taxDeferredInvestment.name }}</h3>
+          <CommonButton @click="deleteInvestment(investmentIndex)">
+            <TrashIcon/>
+          </CommonButton>
         </div>
-        <!--        <FormToggle v-model="taxDeferredInvestment" :field="fieldMetadata.employerContributes"/>-->
-        {{ taxDeferredInvestment.employerContributes }}
-      </section>
-      <section v-if="taxDeferredInvestment.employerContributes">
-        <h3 class="text-xl">Employer Contributions</h3>
         <div class="grid grid-cols-3 gap-3">
-          <FormSelect :model="taxDeferredInvestment" :field="fieldMetadata.employerContributionStrategy"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerCompensationMatchPercentage"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerContributionFixedAmount"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerMatchPercentage"/>
-          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerMatchPercentageLimit"/>
+          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.name"/>
+          <FormField :model="taxDeferredInvestment" :field="fieldMetadata.balance"/>
+          <FormField :model="taxDeferredInvestment" v-show="showAdvancedOptions" :field="fieldMetadata.growthRate"/>
         </div>
-      </section>
-    </Form>
-  </CommonCard>
+        <section>
+          <h3 class="text-xl">Elective Contributions</h3>
+          <div class="grid grid-cols-3 gap-3">
+            <FormSelect :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionStrategy"/>
+            <FormField :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionPercentage"/>
+            <FormField :model="taxDeferredInvestment" :field="fieldMetadata.electiveContributionFixedAmount"/>
+          </div>
+          <FormToggle :model="taxDeferredInvestment" :field="fieldMetadata.employerContributes"/>
+        </section>
+        <transition
+            name="custom-classes"
+            enter-active-class="animate__animated animate__fadeInDown"
+            leave-active-class="animate__animated animate__fadeInUp"
+        >
+          <section v-if="taxDeferredInvestment.employerContributes">
+            <h3 class="text-xl">Employer Contributions</h3>
+            <div class="grid grid-cols-3 gap-3">
+              <FormSelect :model="taxDeferredInvestment" :field="fieldMetadata.employerContributionStrategy"/>
+              <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerCompensationMatchPercentage"/>
+              <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerContributionFixedAmount"/>
+              <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerMatchPercentage"/>
+              <FormField :model="taxDeferredInvestment" :field="fieldMetadata.employerMatchPercentageLimit"/>
+            </div>
+          </section>
+        </transition>
+      </Form>
+    </CommonCard>
 </template>
 <script setup lang="ts">
 import TaxDeferredInvestment from "~/models/TaxDeferredInvestment";
