8613e5767d2ce9f43bfb050b8215e8aa9c0b0dac
Marek Schir <schir2@gmail.com>
2024-11-19 19:55:32 -0500

    renamed DEFAULT_PLAN_NAME to DEFAULT_EXPENSE_PLAN_NAME

--- constants/plan.ts
+++ constants/plan.ts
@@ -4,17 +4,20 @@ import Income from "~/models/Income";
 import Debt from "~/models/Debt";
 import Cash from "~/models/Cash";
 import TaxDeferredInvestment from "~/models/TaxDeferredInvestment";
-import ExpensePlan from "~/models/ExpensePlan";
+import ExpensePlan, {ExpensePlanType} from "~/models/ExpensePlan";
 
-export const DEFAULT_PLAN_NAME: string = 'Blank Plan'
+export const DEFAULT_EXPENSE_PLAN_NAME: string = 'Blank Plan'
+
+const DEFAULT_EXPENSE_PLAN_ACTIVE_EXPENSE_PLAN = ExpensePlanType.Simple;
 
 export const PLAN_TEMPLATE: Record<string, PlanData> = {
     default: {
-        name: DEFAULT_PLAN_NAME,
+        name: DEFAULT_EXPENSE_PLAN_NAME,
         retirement: new Retirement(Retirement.defaultValues()),
         incomes: [Income.defaultValues()],
         simpleExpensePlan: ExpensePlan.defaultValues(),
         itemizedExpensePlan: ExpensePlan.defaultValues('itemized'),
+        activeExpensePlan: DEFAULT_EXPENSE_PLAN_ACTIVE_EXPENSE_PLAN,
         cash: new Cash(Cash.defaultValues()),
         debts: [Debt.defaultValues()],
         taxDeferredInvestments: [TaxDeferredInvestment.defaultValues()],
