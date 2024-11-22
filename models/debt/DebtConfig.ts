bd2c5054af43a8e9a5caf441c6346b4d015be3df
Marek Schir <schir2@gmail.com>
2024-11-22 00:41:19 -0500

    converted config to just be a simple config model

--- models/debt/DebtConfig.ts
+++ models/debt/DebtConfig.ts
@@ -2,6 +2,7 @@ import type {DebtPaymentStrategy} from "~/types";
 import {DEBT_TEMPLATE} from "~/models/debt/constants";
 
 export interface DebtData {
+    id: string;
     name: string;
     principal: number;
     interestRate: number;
@@ -12,78 +13,27 @@ export interface DebtData {
 }
 
 export default class DebtConfig {
+    id: string;
     name: string;
-    principalStartOfYear: number;
-    principalEndOfYear: number;
+    principal: number;
     interestRate: number;
-    interestAmount: number;
-    payment: number;
     paymentFixedAmount: number;
     paymentStrategy: DebtPaymentStrategy;
-    paymentLifetime: number;
     paymentMinimum: number;
     paymentPercentage: number;
-    interestAccrued: number;
 
     constructor(data: DebtData) {
+        this.id = data.id;
         this.name = data.name;
-
+        this.principal = data.principal;
         this.interestRate = data.interestRate;
-        this.interestAmount = 0
-        this.interestAccrued = 0;
-
-        this.payment = 0;
-        this.paymentLifetime = 0;
         this.paymentMinimum = data.paymentMinimum;
         this.paymentStrategy = data.paymentStrategy;
         this.paymentFixedAmount = data.paymentFixedAmount;
         this.paymentPercentage = data.paymentPercentage;
-
-        this.principalStartOfYear = data.principal;
-        this.principalEndOfYear = data.principal;
     }
 
-    calculateEndOfYearValues() {
-        const principalEndOfYear = this.principalStartOfYear - this.payment;
-        const interestAmount = principalEndOfYear * (this.interestRate / 100);
-        const interestAccrued = this.interestAccrued + interestAmount;
-        const paymentLifetime = this.paymentLifetime + this.payment;
-        const updatedPrincipalEndOfYear = principalEndOfYear + interestAmount;
-        return {
-            principalEndOfYear: updatedPrincipalEndOfYear,
-            interestAmount,
-            interestAccrued,
-            paymentLifetime,
-        };
-    }
-
-
-    calculatePayment(disposableIncome: number) {
-        let payment = 0
-        switch (this.paymentStrategy) {
-            case 'fixed':
-                payment = this.paymentFixedAmount;
-                break
-            case 'percentage_of_debt':
-                payment = this.principalStartOfYear * (this.paymentFixedAmount / 100);
-                break
-            case 'max':
-                payment = this.principalStartOfYear;
-                break
-        }
-        payment = Math.max(payment, this.paymentMinimum);
-        return Math.min(payment, disposableIncome, this.principalStartOfYear);
-
-    }
-
-    advanceToNextYear(): DebtConfig {
-        let debt = structuredClone(this)
-        debt.principalStartOfYear = this.principalEndOfYear
-        debt.payment = 0
-        return debt
-    }
-
-    static defaultValues(template?: keyof typeof DEBT_TEMPLATE){
+    static defaultValues(template?: keyof typeof DEBT_TEMPLATE) {
         return DEBT_TEMPLATE[template ?? 'default']
     }
 }
