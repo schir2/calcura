562a3c985ff39ab9c7ee587afaacbe1c75123689
Marek Schir <schir2@gmail.com>
2024-11-22 00:43:07 -0500

    added initial plan state

---
+++ models/plan/PlanState.ts
@@ -0,0 +1,12 @@
+export default interface PlanState {
+    age: number;
+    year: number;
+    grossIncome: number;
+    disposableIncome: number;
+    electiveLimit: number;
+    deferredLimit: number;
+    iraLimit: number;
+    inflationRate: number;
+    savingsStartOfYear: number;
+    endOfYearSavings: number;
+}
