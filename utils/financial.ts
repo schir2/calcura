dd970f19a07f32a4d143b0cf4550d4a21154df73
Marek Schir <schir2@gmail.com>
2024-11-22 00:42:13 -0500

    added new helper function that adjusts for disposable income

--- utils/financial.ts
+++ utils/financial.ts
@@ -1,4 +1,5 @@
 import type {InvestmentGrowthApplicationStrategy} from "~/types";
+import {AllowNegativeDisposableIncome} from "~/models/plan/PlanConfig";
 
 export function calculateCompoundInterest(principal: number, interestRate: number, numberOfInterestApplicationsPerPeriod: number = 1, numberOfPeriods: number): number {
     return principal * (1 + (interestRate / numberOfInterestApplicationsPerPeriod)) ** (numberOfInterestApplicationsPerPeriod * numberOfPeriods)
@@ -48,5 +49,22 @@ export function adjustContributionForDisposableIncome({
             return Math.max(amount, minimum);
         default:
             throw new Error(`Invalid allowNegativeDisposableIncome value: ${allowNegativeDisposableIncome}`);
+    }
+}
+
+
+export function adjustForAllowNegativeDisposableIncome(
+    {disposableIncome, amount, minimum = 0, allowNegative}: {
+        disposableIncome: number, amount: number, minimum?: number, allowNegative: AllowNegativeDisposableIncome }
+): number {
+    switch (allowNegative) {
+        case AllowNegativeDisposableIncome.none:
+            return Math.min(amount, disposableIncome, minimum);
+        case AllowNegativeDisposableIncome.minimumOnly:
+            return Math.min(amount, disposableIncome + minimum, minimum);
+        case AllowNegativeDisposableIncome.full:
+            return amount;
+        default:
+            throw new Error(`Unknown AllowNegativeDisposableIncome option: ${allowNegative}`);
     }
 }
