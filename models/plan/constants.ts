ac4b8ed72f9c1da7257c8179408cc7b66176cd0c
Marek Schir <schir2@gmail.com>
2024-11-22 00:43:25 -0500

    moved plan variables to the plan

--- models/plan/constants.ts
+++ models/plan/constants.ts
@@ -3,16 +3,19 @@ import RetirementConstants from "~/models/retirement/RetirementConstants";
 import IncomeConfig from "~/models/income/IncomeConfig";
 import DebtConfig from "~/models/debt/DebtConfig";
 import CashConfig from "~/models/cash/CashConfig";
-import TaxDeferredInvestmentConfig from "~/models/taxDeferred/config";
+import TaxDeferredInvestmentConfig from "~/models/taxDeferred/TaxDeferredInvestmentConfig";
 import ExpensePlanConfig, {ExpensePlanType} from "~/models/expense/ExpensePlanConfig";
 
 export const DEFAULT_EXPENSE_PLAN_NAME: string = 'Blank Plan'
+export const DEFAULT_AGE: number = 30;
+export const DEFAULT_YEAR: number = new Date().getFullYear();
 
 const DEFAULT_EXPENSE_PLAN_ACTIVE_EXPENSE_PLAN = ExpensePlanType.Simple;
 
 export const PLAN_TEMPLATE: Record<string, PlanData> = {
     default: {
         name: DEFAULT_EXPENSE_PLAN_NAME,
+        age: DEFAULT_AGE,
         retirement: new RetirementConstants(RetirementConstants.defaultValues()),
         incomes: [IncomeConfig.defaultValues()],
         simpleExpensePlan: ExpensePlanConfig.defaultValues(),
