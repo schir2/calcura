560f2489a3af77385b59fe2747f92a7ed1fe67fb
Marek Schir <schir2@msn.com>
2024-11-24 16:25:25 -0500

    renamed constants file and added new props to plan data - year, inflationRate, allowNegativeDisposableIncome and cash -> cashes

--- models/plan/PlanConfig.ts
+++ models/plan/PlanConfig.ts
@@ -5,7 +5,7 @@ import type {IncomeData} from "~/models/income/IncomeConfig"
 import IncomeConfig from "~/models/income/IncomeConfig";
 import type {RetirementData} from "~/models/retirement/RetirementConstants";
 import RetirementConstants from "~/models/retirement/RetirementConstants";
-import type {INCOME_TEMPLATE} from "~/models/income/constants";
+import type {INCOME_TEMPLATE} from "~/models/income/IncomeConstants";
 import {PLAN_TEMPLATE} from "~/models/plan/constants";
 import {TAX_DEFERRED_INVESTMENT_TEMPLATE} from "~/models/taxDeferred/TaxDeferredInvestmentConstants";
 import {DEBT_TEMPLATE} from "~/models/debt/DebtConstants";
@@ -14,11 +14,22 @@ import CashConfig from "~/models/cash/CashConfig";
 import type {ExpensePlanData} from "~/models/expense/ExpensePlanConfig";
 import ExpensePlanConfig, {ExpensePlanType} from "~/models/expense/ExpensePlanConfig";
 
+
+export enum AllowNegativeDisposableIncome {
+    none = 'none',
+    minimumOnly = 'minimum_only',
+    full = 'full'
+}
+
 export interface PlanData {
     name: string;
     age: number;
+    year: number;
+    inflationRate: number;
+    allowNegativeDisposableIncome: AllowNegativeDisposableIncome
+
     retirement: RetirementData
-    cash: CashData
+    cashes: CashData[]
     incomes: IncomeData[]
     simpleExpensePlan: ExpensePlanData
     itemizedExpensePlan: ExpensePlanData
@@ -30,8 +41,11 @@ export interface PlanData {
 export default class PlanConfig {
     name: string;
     age: number;
+    year: number;
+    inflationRate: number;
+    allowNegativeDisposableIncome: AllowNegativeDisposableIncome;
     retirement: RetirementConstants
-    cash: CashConfig
+    cashes: CashConfig[]
     incomes: IncomeConfig[]
     simpleExpensePlan: ExpensePlanConfig
     itemizedExpensePlan: ExpensePlanConfig
@@ -42,12 +56,15 @@ export default class PlanConfig {
     constructor(data: PlanData) {
         this.name = data.name;
         this.age = data.age;
+        this.year = data.year;
+        this.inflationRate = data.inflationRate;
+        this.allowNegativeDisposableIncome = data.allowNegativeDisposableIncome;
         this.retirement = new RetirementConstants(data.retirement)
         this.incomes = data.incomes.map((income) => new IncomeConfig(income))
         this.simpleExpensePlan = new ExpensePlanConfig(data.simpleExpensePlan)
         this.itemizedExpensePlan = new ExpensePlanConfig(data.itemizedExpensePlan)
         this.activeExpensePlan = data.activeExpensePlan
-        this.cash = new CashConfig(data.cash)
+        this.cashes = data.cashes.map((cash) => new CashConfig(cash))
         this.debts = data.debts.map((debt) => new DebtConfig(debt))
         this.taxDeferredInvestments = data.taxDeferredInvestments.map((taxDeferredInvestment) => new TaxDeferredInvestmentConfig(taxDeferredInvestment))
     }
@@ -85,10 +102,4 @@ export default class PlanConfig {
     static defaultValues(template?: keyof typeof PLAN_TEMPLATE): PlanData {
         return PLAN_TEMPLATE[template ?? 'default']
     }
-}
-
-export enum AllowNegativeDisposableIncome {
-    none = 'none',
-    minimumOnly = 'minimum_only',
-    full = 'full'
 }
