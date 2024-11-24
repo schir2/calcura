eda7a2939d04a60e9bc9a09b4fbd97ee28334210
Marek Schir <schir2@msn.com>
2024-11-24 01:52:42 -0500

    added debt command

---
+++ models/debt/DebtCommands.ts
@@ -0,0 +1,19 @@
+import type Command from "~/models/common/Command";
+import DebtManager from "~/models/debt/DebtManager";
+import type PlanState from "~/models/plan/PlanState";
+
+export class ProcessDebtCommand implements Command {
+    private debtManager: DebtManager;
+
+    constructor(debtManager: DebtManager) {
+        this.debtManager = debtManager;
+    }
+
+    execute(planState: PlanState): PlanState {
+        return this.debtManager.process(planState);
+    }
+
+    getName(): string {
+        return `Process Debt: ${this.debtManager.getConfig().name}`;
+    }
+}
