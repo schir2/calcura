8ddb02464a8cfae46e36b1542772c850be192604
Marek Schir <schir2@msn.com>
2024-11-24 01:53:39 -0500

    added a dummy test page for debt

--- pages/debt/index.vue
+++ pages/debt/index.vue
@@ -1,33 +1,93 @@
 <template>
   <div class="container">
     <PlanDebt :debtIndex=1 :debt="debtConfig" :showAdvancedOptions="false "/>
-    <table>
-      <tbody>
-      <tr v-for></tr>
-      </tbody>
-    </table>
+    <div class="grid grid-cols-2 gap-6">
+      <table>
+        <thead>
+        <tr>
+          <th>payment</th>
+          <th>principalStartOfYear</th>
+          <th>interestAmount</th>
+          <th>principalEndOfYear</th>
+          <th>processed</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(state, index) in debtManager.getStates()">
+          <td>{{ state.payment }}</td>
+          <td>{{ state.principalStartOfYear }}</td>
+          <td>{{ state.interestAmount }}</td>
+          <td>{{ state.principalEndOfYear }}</td>
+          <td>{{ state.processed }}</td>
+        </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+        <tr>
+          <th>Disposable Income</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(state, index) in planStates">
+          <td>{{ state.disposableIncome }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import DebtConfig from "~/models/debt/DebtConfig";
-import DebtManager from "~/models/debt/DebtManager";
-import {AllowNegativeDisposableIncome} from "~/models/plan/PlanConfig";
+import { reactive, ref, watch } from 'vue';
+import DebtConfig from '~/models/debt/DebtConfig';
+import DebtManager from '~/models/debt/DebtManager';
+import { AllowNegativeDisposableIncome } from '~/models/plan/PlanConfig';
+import type PlanState from '~/models/plan/PlanState';
+import type DebtState from "~/models/debt/DebtState";
 
-const debtConfig = ref(new DebtConfig(DebtConfig.defaultValues()))
-const debtManager = ref(new DebtManager(debtConfig.value))
-const allowNegativeIncome = AllowNegativeDisposableIncome.none
-const MAX_ITERATIONS = 30
-watch(debtConfig.value, (oldValue, newValue) => {
-  let i = 0
-  let disposableIncome = 100000
-  console.log(newValue)
-  debtManager.value = new DebtManager(newValue)
+const debtConfig = reactive(new DebtConfig(DebtConfig.defaultValues()));
+const debtManager = ref(new DebtManager(debtConfig));
+const cmd = debtManager.value.getCommands()
+const planStates = ref<PlanState[]>([]);
+const MAX_ITERATIONS = 30;
+
+watch(debtConfig, (newValue) => {
+  let i = 0;
+  debtManager.value = new DebtManager(newValue);
+  let planState: PlanState = {
+    age: 30,
+    year: 2024,
+    grossIncome: 100000,
+    disposableIncome: 70000,
+    electiveLimit: 22500,
+    deferredLimit: 66000,
+    iraLimit: 7000,
+    inflationRate: 3,
+    savingsStartOfYear: 0,
+    endOfYearSavings: 0,
+    allowNegativeDisposableIncome: AllowNegativeDisposableIncome.none,
+  };
+  planStates.value = [planState];
   while (i < MAX_ITERATIONS) {
-    const {payment} = debtManager.value.process(disposableIncome, allowNegativeIncome)
-    disposableIncome -= payment
-    debtManager.value.advanceToNextYear()
-    i += 1
-  }
+    debtManager.value.getCommands().forEach((c) => {
+      planStates.value.push(c.execute(planState))
+    })
+    const currentState: DebtState = debtManager.value.getCurrentState();
+    // planStates.value.push(planState);
+    if (currentState.principalEndOfYear !== undefined && currentState.principalEndOfYear <= 0) {
+      console.log("Debt fully paid off. Stopping iteration.");
+      break;
 
-})
+    }
+    if (planState.disposableIncome <= 0) {
+      console.log("No more disposable income. Stopping iteration.");
+      break;
+    }
+    if (i == MAX_ITERATIONS - 1){
+      break;
+    }
+    debtManager.value.advanceTimePeriod();
+    i += 1;
+  }
+}, { deep: true });
 </script>
