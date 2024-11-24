2822500197355cca995e37b47774984dd25a4d40
Marek Schir <schir2@msn.com>
2024-11-24 01:59:08 -0500

    added debt manager into the plan manager

--- models/plan/PlanManager.ts
+++ models/plan/PlanManager.ts
@@ -1,12 +1,15 @@
 import type PlanConfig from "~/models/plan/PlanConfig";
 import type PlanState from "~/models/plan/PlanState";
+import DebtManager from "~/models/debt/DebtManager";
 
 export default class PlanManager {
     private config: PlanConfig
     planStates: PlanState[] = []
+    debtManagers: DebtManager[]
 
     constructor(config: PlanConfig) {
         this.config = config
+        this.debtManagers = config.debts.map((debtConfig)=>new DebtManager(debtConfig));
 
     }
 }
