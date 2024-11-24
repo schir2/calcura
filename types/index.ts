b5962762204e3cd540d0ede2b5597761f496a0f1
Marek Schir <schir2@msn.com>
2024-11-24 10:42:27 -0500

    reverted back to type for DebtPaymentStrategy

--- types/index.ts
+++ types/index.ts
@@ -6,5 +6,4 @@ export type ExpensesGrowthStrategy = 'fixed' | 'percentage_increase';
 export type IraContributionStrategy = 'fixed' | 'percent_of_income' | 'max';
 export type InvestmentGrowthApplicationStrategy = 'start' | 'end';
 export type TaxableContributionStrategy = 'fixed' | 'percent_of_income';
-export type DebtPaymentStrategy = 'fixed' | 'max' | 'percentage_of_debt'
 
