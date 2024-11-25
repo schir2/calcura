daa1d177ef98f8bfc6901d28fd8712feaffd2c1b
Marek Schir <schir2@gmail.com>
2024-11-24 21:39:11 -0500

    added income manager

---
+++ models/income/IncomeManager.ts
@@ -0,0 +1,36 @@
+import ManagerBase from "~/models/common/ManagerBase";
+import type IncomeState from "~/models/income/IncomeState";
+import IncomeConfig from "~/models/income/IncomeConfig";
+import type PlanState from "~/models/plan/PlanState";
+import type Command from "~/models/common/Command";
+
+export default class IncomeManager extends ManagerBase<IncomeConfig, IncomeState> {
+    protected createInitialState(): IncomeState {
+        return {
+            grossIncome: this.config.grossIncome
+        }
+    }
+
+    protected createNextState(previousState: IncomeState): IncomeState {
+        return {
+            grossIncome: previousState.grossIncome + this.getGrowthAmount(previousState.grossIncome)
+        }
+    }
+
+    protected getGrowthAmount(grossIncome: number): number {
+        return grossIncome * this.config.growthRate / 100
+    }
+
+    getCommands(): Command[] {
+        return [];
+    }
+
+    process(planState: PlanState): PlanState {
+        const grossIncome = this.getCurrentState().grossIncome
+        return {
+            ...planState,
+            grossIncome: planState.grossIncome + grossIncome
+        };
+    }
+
+}
