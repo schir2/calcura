f52b6e503553b24ad4465e64cdb010fbba09d46c
Marek Schir <schir2@gmail.com>
2024-11-30 15:52:16 -0500

    added coverage?

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,9 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-    // any custom Vitest config you require
+    test: {
+        coverage: {
+            provider: 'v8' // or 'v8'
+        },
+    },
 })
