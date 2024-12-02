e70f22b84e47821b31729b2ee3b10086d769ac18
Marek Schir <schir2@gmail.com>
2024-12-01 22:03:34 -0500

    added nuxt icons

--- components/common/Button.vue
+++ components/common/Button.vue
@@ -1,22 +1,21 @@
-<script setup lang="ts">
-interface Props {
-  disabled?: boolean,
-}
-
-const {disabled = false} = defineProps<Props>()
-
-</script>
-
 <template>
-  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    <slot name="leftIcon"></slot>
+  <button type="button" class="text-white bg-skin-info hover:bg-skin-info/80 inline-flex items-center gap-3 focus:outline-none focus:ring-4 focus:ring-skin-info font-medium rounded-full px-5 py-2.5 text-center me-2 mb-2">
+    <Icon v-if="iconLeft" :name="iconLeft"></Icon>
     <slot></slot>
-    <slot name="rightIcon"></slot>
+    <Icon v-if="iconRight" :name="iconRight"></Icon>
   </button>
 
 
+
 </template>
+<script setup lang="ts">
 
-<style scoped>
+interface Props {
+  disabled?: boolean,
+  iconLeft?: string
+  iconRight?: string
+}
 
-</style>
+const props = defineProps<Props>()
+
+</script>
