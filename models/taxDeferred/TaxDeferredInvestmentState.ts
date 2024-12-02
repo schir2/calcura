4fec8abc218899469a0977f6bde470d330723938
Marek Schir <schir2@gmail.com>
2024-12-01 22:08:37 -0500

    added processed prop

--- models/taxDeferred/TaxDeferredInvestmentState.ts
+++ models/taxDeferred/TaxDeferredInvestmentState.ts
@@ -1,32 +1,8 @@
-export interface TaxDeferredInvestmentState {
-    year: number;
-    balanceStartOfYear: number;
-
-    electiveContribution: number;
-    employerContribution: number;
-
+export default interface TaxDeferredInvestmentState {
+    electiveContribution?: number;
+    employerContribution?: number;
     growthAmount: number;
-    balanceEndOfYear: number;
-
-    // Optional: Add any other fields you need for tracking
-}
-
-export class TaxDeferredInvestmentState {
-    year: number;
     balanceStartOfYear: number;
-    electiveContribution: number;
-    employerContribution: number;
-    growthAmount: number;
     balanceEndOfYear: number;
-
-    constructor(data: TaxDeferredInvestmentState) {
-        this.year = data.year;
-        this.balanceStartOfYear = data.balanceStartOfYear;
-
-        this.electiveContribution = data.electiveContribution;
-        this.employerContribution = data.employerContribution;
-
-        this.growthAmount = data.growthAmount;
-        this.balanceEndOfYear = data.balanceEndOfYear;
-    }
+    processed: boolean;
 }
