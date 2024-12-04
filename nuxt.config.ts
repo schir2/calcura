34c8d435943b30293f9d73d095cf30475960304c
Marek Schir <schir2@gmail.com>
2024-12-04 15:46:53 -0500

    added initial prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@vueuse/nuxt',
         '@nuxt/icon',
-        '@nuxt/prisma',
+        '@prisma/nuxt',
     ],
     icon:{
         serverBundle: {
