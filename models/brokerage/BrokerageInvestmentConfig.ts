2add0f0e600bd1cc194c776b19c323f7fb8384fe
Marek Schir <schir2@gmail.com>
2024-12-04 17:39:56 -0500

    k

--- models/brokerage/BrokerageInvestmentConfig.ts
+++ models/brokerage/BrokerageInvestmentConfig.ts
@@ -1,10 +1,7 @@
-import type {InvestmentGrowthApplicationStrategy} from "~/types";
-
 export type BrokerageContributionStrategy = 'fixed' | 'percentage_of_income' | 'max'
 
 export default interface BrokerageInvestmentConfig {
     name: string;
-    growthApplicationStrategy: InvestmentGrowthApplicationStrategy;
 
     growthRate: number;
     initialBalance: number;
