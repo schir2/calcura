2d266541f1fd8a6765cd75204a060063bbee1f90
Marek Schir <schir2@gmail.com>
2024-12-07 17:41:39 -0500

    moved debt into its own directory

---
+++ components/debt/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <CommonCard color="secondary">
+    <div class="flex justify-between align-middle">
+      <h3 class="text-2xl">DebtConfig {{ debtConfig.id }}: {{ currentDebtConfig.name }}</h3>
+      <CommonButton iconLeft="mdi:delete" @click="deleteDebt">Delete</CommonButton>
+      <CommonButton v-if="isModified" iconLeft="mdi:history" @click="resetDebt">Reset</CommonButton>
+      <CommonButton v-if="isModified" iconLeft="mdi:content-save" @click="updateDebt">Save</CommonButton>
+    </div>
+    <Form>
+      <section>
+        <div class="grid grid-cols-6 gap-3">
+          <FormField :field="fieldMetadata.name" :model="currentDebtConfig"></FormField>
+          <FormField :field="fieldMetadata.principal" :model="currentDebtConfig"></FormField>
+          <FormField :field="fieldMetadata.interestRate" :model="currentDebtConfig"></FormField>
+          <FormSelect :field="fieldMetadata.paymentStrategy" :model="currentDebtConfig"></FormSelect>
+          <FormField :field="fieldMetadata.paymentMinimum" :model="currentDebtConfig"></FormField>
+          <FormField v-if="currentDebtConfig.paymentStrategy === 'percentage_of_debt'" :field="fieldMetadata.paymentPercentage" :model="currentDebtConfig"></FormField>
+          <FormField v-if="currentDebtConfig.paymentStrategy ==='fixed'" :field="fieldMetadata.paymentFixedAmount" :model="currentDebtConfig"></FormField>
+        </div>
+      </section>
+    </Form>
+  </CommonCard>
+
+</template>
+<script setup lang="ts">
+import {debtFields} from "~/forms/debtForm";
+import type DebtConfig from "~/models/debt/DebtConfig";
+
+interface Props {
+  debtConfig: DebtConfig
+  showAdvancedOptions?: boolean;
+}
+
+const {showAdvancedOptions = false, debtConfig} = defineProps<Props>()
+const fieldMetadata = debtFields
+
+const emit = defineEmits(['deleteDebt', 'updateDebt']);
+
+function deleteDebt() {
+  assertDefined(debtConfig.id, 'debtId')
+  emit('deleteDebt', debtConfig.id)
+}
+
+function updateDebt() {
+  assertDefined(debtConfig.id, 'debtId')
+  emit('updateDebt', debtConfig)
+}
+
+const currentDebtConfig = reactive({ ...debtConfig });
+const isModified = computed(() =>
+    JSON.stringify(currentDebtConfig) !== JSON.stringify(debtConfig)
+);
+
+function resetDebt() {
+  Object.assign(currentDebtConfig, { ...debtConfig });
+}
+
+</script>
