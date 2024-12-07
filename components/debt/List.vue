0479de00c50209d4049d65b78e88a0f09b5e0972
Marek Schir <schir2@gmail.com>
2024-12-07 17:41:59 -0500

    created a debt list component

---
+++ components/debt/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <CommonButton @click="handleCreateDebt()">Add</CommonButton>
+  <div class="container">
+    <Debt v-for="(debtConfig, index) in debts" :debtConfig="debtConfig" :key="debtConfig.id"
+          @deleteDebt="handleDeleteDebt" @updateDebt="handleUpdateDebt"></Debt>
+  </div>
+
+</template>
+<script lang="ts" setup>
+interface Props {
+  debts: DebtConfig[]
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits(['deleteDebt', 'updateDebt', 'createDebt']);
+function handleDeleteDebt(debtId: number){
+  emit('deleteDebt',debtId);
+}
+function handleCreateDebt(){
+  emit('createDebt');
+}
+
+function handleUpdateDebt(debtConfig: debtConfig) {
+  emit('updateDebt', debtConfig);
+}
+
+</script>
