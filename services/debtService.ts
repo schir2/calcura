1e4e7e1c2d59b46c0dbcc5ad525b51d14c3ce0b1
Marek Schir <schir2@gmail.com>
2024-12-07 17:41:20 -0500

    converted debt service into a composable

--- services/debtService.ts
+++
@@ -1,4 +0,0 @@
-import { createBaseService } from './baseService'
-import type DebtConfig from "~/models/debt/DebtConfig";
-
-export const debtService = createBaseService<DebtConfig>('http://localhost:8000/debt-configs/')
