2743154f9fe150e822aba0b70521dfc63fb2e1ca
Marek Schir <schir2@gmail.com>
2024-12-09 23:28:27 -0500

    moved types and constants

--- composables/planService.ts
+++ composables/planService.ts
@@ -1,7 +1,36 @@
 import {createBaseService} from '~/services/baseService';
 import type {Plan} from "~/models/plan/Plan";
+import {ofetch} from "ofetch";
+
+type PlanRelatedModel =
+    'Incomes'
+    | 'Expenses'
+    | 'Cashes'
+    | 'IraInvestments'
+    | 'TaxDeferredInvestments'
+    | 'BrokerageInvestments'
 
 export function usePlanService() {
     const config = useRuntimeConfig();
-    return createBaseService<Plan>(config.public.apiBaseUrl, 'plan-configs/');
+    const baseService = createBaseService<Plan>(config.public.apiBaseUrl, 'plan/');
+
+    return baseService.extend({
+        async addRelatedModel(
+            planId: number | string,
+            relatedModel: PlanRelatedModel,
+            relatedId: number | string
+        ): Promise<Plan> {
+            return await ofetch(
+                `${config.public.apiBaseUrl}plan/${planId}/manage_related_model/`,
+                {
+                    method: 'POST',
+                    body: {
+                        related_model: relatedModel,
+                        related_id: relatedId,
+                        action: 'add',
+                    }
+                }
+            )
+        }
+    })
 }
