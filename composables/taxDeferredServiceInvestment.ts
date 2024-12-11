c1b6d79c6e8a1041b548872a59ff525d49e698ff
Marek Schir <schir2@msn.com>
2024-12-11 11:59:57 -0500

    updated code to be in line with the server

--- composables/taxDeferredServiceInvestment.ts
+++ composables/taxDeferredServiceInvestment.ts
@@ -1,11 +1,11 @@
 import {createBaseService} from '~/services/baseService';
 import type {TaxDeferredInvestment} from "~/models/taxDeferred/TaxDeferredInvestment";
 
-export function useDebtService() {
+export function useTaxDeferredInvestmentService() {
     const {$api} = useNuxtApp();
     if (!$api) {
         throw new Error('API service is not available');
     }
 
-    return createBaseService<TaxDeferredInvestment>($api, 'tax_deferred_incomes/');
+    return createBaseService<TaxDeferredInvestment>($api, 'tax_deferred_investments/');
 }
