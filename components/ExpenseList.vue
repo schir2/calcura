c5d296cf4ca87a39908b43f55a06d7cb7f9f19db
Marek Schir <schir2@msn.com>
2024-12-11 12:01:23 -0500

    removed config from names

---
+++ components/ExpenseList.vue
@@ -0,0 +1,29 @@
+<template>
+  <NButton @click="handleCreateExpense()">Add</NButton>
+  <div class="container">
+    <Expense v-for="(expense, index) in expenses" :expense="expense" :key="expense.id"
+             @deleteExpense="handleDeleteExpense" @updateExpense="handleUpdateExpense"></Expense>
+  </div>
+
+</template>
+<script lang="ts" setup>
+import type {Expense} from "~/models/expense/Expense"
+interface Props {
+  expenses: Expense[]
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits(['deleteExpense', 'updateExpense', 'createExpense']);
+function handleDeleteExpense(expenseId: number){
+  emit('deleteExpense',expenseId);
+}
+function handleCreateExpense(){
+  emit('createExpense');
+}
+
+function handleUpdateExpense(expense: Expense) {
+  emit('updateExpense', expense);
+}
+
+</script>
