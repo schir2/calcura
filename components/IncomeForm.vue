8c7d48d3bfa75e7a0653500faec8b056230a3e3f
Marek Schir <schir2@msn.com>
2024-12-15 18:44:32 -0500

    implemented income form

--- components/IncomeForm.vue
+++ components/IncomeForm.vue
@@ -1,17 +1,108 @@
 <template>
-<Form>
-  <FormTextInput :field="incomeFields.name" :modelValue="incomePartial"></FormTextInput>
-  <FormSelect :field="incomeFields.incomeType" :model="incomePartial"></FormSelect>
-  <FormSelect :field="incomeFields.frequency" :model="incomePartial"></FormSelect>
-  <FormTextInput :field="incomeFields.grossIncome" :modelValue="incomePartial"></FormTextInput>
-  <FormTextInput :field="incomeFields.growthRate" :modelValue="incomePartial"></FormTextInput>
-</Form>
+  <n-card role="dialog" class="max-w-2xl" :bordered="true">
+    <template #header>
+      <h3 class="text-2xl">Income: {{ incomePartial.name }}</h3>
+    </template>
+
+    <template #default>
+      <n-form>
+        <n-form-item path="name" label="Name" v-bind="nameProps">
+          <n-input v-model:value="name"/>
+        </n-form-item>
+
+        <n-form-item path="incomeType" label="Income Type" v-bind="incomeTypeProps">
+          <n-select v-model:value="incomeType" :options="incomeFields.incomeType.options"/>
+        </n-form-item>
+
+        <n-form-item path="frequency" label="Frequency" v-bind="frequencyProps">
+          <n-select v-model:value="frequency" :options="incomeFields.frequency.options"/>
+        </n-form-item>
+
+        <n-form-item path="grossIncome" label="Gross Income" v-bind="grossIncomeProps">
+          <n-input-number v-model:value="grossIncome"/>
+        </n-form-item>
+
+        <n-form-item path="growthRate" label="Growth Rate" v-bind="growthRateProps">
+          <n-input-number v-model:value="growthRate" suffix="%"/>
+        </n-form-item>
+      </n-form>
+    </template>
+
+    <template #action>
+      <n-button-group>
+        <n-button secondary round v-if="mode ==='edit'" type="success" @click="handleUpdate">
+          <template #icon>
+            <Icon name="mdi:content-save"/>
+          </template>
+          <span>Save</span>
+        </n-button>
+        <n-button secondary round v-if="mode==='edit'" type="warning" @click="handleCreate">
+          <template #icon>
+            <Icon name="mdi:content-duplicate"/>
+          </template>
+          <span>Duplicate</span>
+        </n-button>
+        <n-button secondary round v-if="mode==='create'" type="success" @click="handleCreate">
+          <template #icon>
+            <Icon name="mdi:content-save"/>
+          </template>
+          <span>Create</span>
+        </n-button>
+        <n-button secondary round type="error" @click="handleCancel">
+          <template #icon>
+            <Icon name="mdi:close"/>
+          </template>
+          Cancel
+        </n-button>
+      </n-button-group>
+    </template>
+  </n-card>
 </template>
+
 <script lang="ts" setup>
-import type {IncomePartial} from "~/models/income/Income";
-const props = defineProps<Props>()
-import {incomeFields} from "~/forms/incomeForm";
+import {incomeFields, incomeFormSchema} from "~/forms/incomeForm";
+import {useForm} from "vee-validate";
+import type {Income} from "~/models/income/Income";
+
 interface Props {
-  incomePartial: IncomePartial
+  incomePartial: Partial<Income>;
+  mode: 'create' | 'edit'
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["update", "cancel", "create"]);
+
+const {defineField, values, errors, handleSubmit, meta} = useForm({
+  validationSchema: incomeFormSchema,
+  initialValues: props.incomePartial,
+});
+
+
+const naiveConfig = (state) => ({
+  props: {
+    validationStatus: state.errors[0] ? "error" : undefined,
+    feedback: state.errors[0],
+  },
+});
+
+
+const [name, nameProps] = defineField("name", naiveConfig);
+const [incomeType, incomeTypeProps] = defineField("incomeType", naiveConfig);
+const [frequency, frequencyProps] = defineField("frequency", naiveConfig);
+const [grossIncome, grossIncomeProps] = defineField("grossIncome", naiveConfig);
+const [growthRate, growthRateProps] = defineField("growthRate", naiveConfig);
+
+
+function handleCreate() {
+  emit('create', values)
+
+}
+
+function handleCancel() {
+  emit('cancel')
+}
+
+function handleUpdate() {
+  emit('update', values)
 }
 </script>
