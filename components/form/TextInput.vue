f334d7b63f08f02e0908af6c7dfa5260ba67a0b8
Marek Schir <schir2@msn.com>
2024-12-15 18:40:03 -0500

    integrated n-input into text input

--- components/form/TextInput.vue
+++ components/form/TextInput.vue
@@ -2,11 +2,12 @@
   <div class="flex flex-col gap-1">
 
     <FormLabel v-if="field.label" :label="field.label"/>
-    <n-input :name="field.name"
-             v-model:value="value"
+    <Field :name="field.name">
+    <n-input v-model:value="value"
              :placeholder="field.placeholder"
              v-bind="naiveInputProps"
     />
+    </Field>
     <FormHelpText v-if="field.helpText" :helpText="field.helpText"></FormHelpText>
     <ErrorMessage :name="field.name" class="block text-skin-error text-sm mt-1"/>
   </div>
@@ -17,8 +18,10 @@ import type {Field} from '~/interfaces/FieldData';
 
 interface NaiveUiProps {
   autoSize?: boolean;
+  bordered?: boolean;
   clearable?: boolean;
   defaultValue?: string;
+  disabled?: string;
   loading?: boolean;
   maxlength?: number;
   minlength?: number;
@@ -36,18 +39,12 @@ interface Props<T = any> {
   modelValue?: T;
 }
 
-const props = defineProps<Props & NaiveUiProps>();
-const {value, errorMessage} = useField(props.field.name, props.field.rules, {
-  initialValue: props.modelValue
-})
 
-const emit = defineEmits(['update:modelValue'])
-watch(value, (newValue) => {
-  emit('update:modelValue', newValue)
-})
+const props = defineProps<Props & NaiveUiProps>();
+const value = toRef(props.modelValue);
 
 const naiveInputProps = computed(() => {
   const { field, modelValue, ...rest } = props;
-  return rest; // Forward remaining props to n-input
+  return rest;
 });
 </script>
