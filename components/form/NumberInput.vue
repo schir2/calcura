c26d423703c0d9c7d76d05b92d27c4065a0b0f52
Marek Schir <schir2@msn.com>
2024-12-15 18:47:13 -0500

    #depracated remove this field

--- components/form/NumberInput.vue
+++ components/form/NumberInput.vue
@@ -5,10 +5,7 @@
     <n-input-number :name="field.name"
                     v-model:value="value"
                     :placeholder="field.placeholder"
-                    :disabled="field.disabled"
-                    :readonly="field.readonly"
-                    :min="min"
-                    :max="max"
+                    v-bind="naiveInputProps"
     />
     <FormHelpText v-if="field.helpText" :helpText="field.helpText"></FormHelpText>
     <ErrorMessage :name="field.name" class="block text-skin-error text-sm mt-1"/>
@@ -16,39 +13,46 @@
 </template>
 <script setup lang="ts">
 import {ErrorMessage} from 'vee-validate';
-import type {FieldData} from '~/interfaces/FieldData';
+import type {Field} from '~/interfaces/FieldData';
+
+interface NaiveUiProps {
+  autoSize?: boolean;
+  bordered?: boolean;
+  clearable?: boolean;
+  defaultValue?: string;
+  disabled?: boolean;
+  format?: any;
+  loading?: boolean;
+  max?: number;
+  min?: number;
+  precision?: number;
+  readonly?: boolean;
+  showButton?: boolean;
+  step?: number;
+  round?: boolean;
+  rows?: number;
+  size?: 'tiny' | 'small' | 'medium' | 'large';
+  status?: 'success' | 'warning' | 'error';
+}
 
 interface Props<T = any> {
-  field: FieldData<T>;
-  model: Record<string, any>;
+  field: Field<T>;
+  modelValue: number;
   type?: 'number' | 'money'
 }
 
-const props = defineProps<Props>();
-const {value} = useField(props.field.name, props.field.rules, {
-  initialValue: props.model[props.field.name]
+const props = defineProps<Props & NaiveUiProps>();
+const {value, errorMessage} = useField(props.field.name, props.field.rules, {
+  initialValue: props.modelValue
 })
 
-function getMinMaxFromRules(rules): Record<'min' | 'max', undefined | number> {
-  let min = undefined;
-  let max = undefined;
-  if (rules && typeof rules.describe === 'function') {
-    const description = rules.describe();
-    if (description.tests) {
-      description.tests.forEach((test: any) => {
-        switch (test.name) {
-          case 'min':
-            min = test.params.min;
-            break;
-          case 'max':
-            max = test.params.max;
-            break;
-        }
-      })
-    }
-  }
-  return {min: min, max: max};
-}
+const emit = defineEmits(['update:modelValue'])
+watch(value, (newValue) => {
+  emit('update:modelValue', newValue)
+})
 
-const {min, max} = getMinMaxFromRules(props.field.rules);
+const naiveInputProps = computed(() => {
+  const {field, modelValue, type, ...rest} = props;
+  return rest;
+});
 </script>
