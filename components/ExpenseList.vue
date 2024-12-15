d74f6dcc9fd45af3b039663ec77f27a1aa00edec
Marek Schir <schir2@msn.com>
2024-12-15 18:43:26 -0500

    updated expense list

--- components/ExpenseList.vue
+++ components/ExpenseList.vue
@@ -1,31 +1,41 @@
 <template>
-
-  <h2 class="text-3xl">Expense(s)</h2>
-  <NButton @click="handleCreateExpense()">Add</NButton>
-  <div class="container">
-    <Expense v-for="(expense, index) in expenses" :expense="expense" :key="expense.id"
-             @deleteExpense="handleDeleteExpense" @updateExpense="handleUpdateExpense"></Expense>
-  </div>
+  <n-card title="Expense(s)">
+    <template #header-extra>
+      <ExpenseTemplatePicker @create="handleCreate"/>
+    </template>
+    <n-list>
+      <ExpenseListItem v-for="(expense, index) in expenses" :expense="expense" :key="expense.id"
+                      @delete="handleDelete" @update="handleUpdate" @create="handleCreate"
+                      @remove="handleRemove"></ExpenseListItem>
+    </n-list>
+  </n-card>
 
 </template>
 <script lang="ts" setup>
-import type {Expense} from "~/models/expense/Expense"
+import type {Expense, ExpenseTemplate} from "~/models/expense/Expense";
+
 interface Props {
   expenses: Expense[]
 }
 
 const props = defineProps<Props>()
 
-const emit = defineEmits(['deleteExpense', 'updateExpense', 'createExpense']);
-function handleDeleteExpense(expenseId: number){
-  emit('deleteExpense',expenseId);
-}
-function handleCreateExpense(){
-  emit('createExpense');
+const emit = defineEmits(['delete', 'update', 'create', 'remove']);
+
+function handleDelete(expense: Expense) {
+  emit('delete', expense);
 }
 
-function handleUpdateExpense(expense: Expense) {
-  emit('updateExpense', expense);
+function handleCreate(expenseTemplate: ExpenseTemplate) {
+  emit('create', expenseTemplate);
+}
+
+function handleUpdate(expense: Expense) {
+  emit('update', expense);
+}
+
+function handleRemove(expense: Expense) {
+  emit('remove', expense)
 }
 
 </script>
