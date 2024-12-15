15cbb668560ec05f449142a7b4c257005cae19c8
Marek Schir <schir2@msn.com>
2024-12-15 18:46:59 -0500

    implemented repetitive buttons for list items

---
+++ components/ListItemButtons.vue
@@ -0,0 +1,23 @@
+<template>
+  <n-button-group size="small">
+    <n-button tertiary round type="warning" @click="$emit('edit')">
+      <template #icon>
+        <Icon name="mdi:edit"/>
+      </template>
+      Edit
+    </n-button>
+    <n-button tertiary round type="error" @click="$emit('delete')">
+      <template #icon>
+        <Icon name="mdi:remove"/>
+      </template>
+      Remove
+
+    </n-button>
+    <n-button secondary round type="error" @click="$emit('remove')">
+      <template #icon>
+        <Icon name="mdi:delete"/>
+      </template>
+      Delete
+    </n-button>
+  </n-button-group>
+</template>
