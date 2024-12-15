8071cf63fb9c26218f2d2ea8acff913362ef62fe
Marek Schir <schir2@msn.com>
2024-12-15 18:47:30 -0500

    converted form to use Naiveui

--- components/PlanForm.vue
+++ components/PlanForm.vue
@@ -1,53 +1,118 @@
 <template>
-    <n-card role="dialog" :bordered="true" aria-modal="true">
-      <template #header>
-        <h3 class="text-2xl">Plan {{ planTemplateRef.id }}: {{ planTemplateRef.name }}</h3>
-      </template>
-      <template #default>
-        <FormTextInput v-model:modelValue="planTemplateRef.name" :field="planForm.name" :maxlength="MAX_NAME_LENGTH"/>
-        <FormNumberInput :model="planTemplateRef" :field="planForm.age"/>
-        <FormNumberInput :model="planTemplateRef" :field="planForm.year"/>
-        <FormNumberInput :model="planTemplateRef" :field="planForm.lifeExpectancy"/>
-        <n-divider/>
-        <n-slider v-model:value="planTemplateRef.taxRate" :marks="sliderMarks"></n-slider>
-        <FormSelect :model="planTemplateRef" :field="planForm.taxStrategy"/>
-        <n-divider/>
-        <FormSelect :model="planTemplateRef" :field="planForm.retirementStrategy"/>
-        {{ planTemplateRef.retirementStrategy }}
-        <FormNumberInput :model="planTemplateRef" v-if="planTemplateRef.retirementStrategy === 'age'" :field="planForm.retirementAge"/>
-        <FormNumberInput :model="planTemplateRef" v-if="planTemplateRef.retirementStrategy === 'target_savings'" :field="planForm.retirementSavingsAmount"/>
-        <FormNumberInput :model="planTemplateRef" v-if="planTemplateRef.retirementStrategy === 'percent_rule'" :field="planForm.retirementWithdrawalRate"/>
-        <FormTextInput :modelValue="planTemplateRef" v-if="planTemplateRef.retirementStrategy === 'percent_rule'" :field="planForm.retirementPlanGoal"/>
-      </template>
+  <n-card role="dialog" class="max-w-2xl" :bordered="true">
+    <template #header>
+      <h3 class="text-2xl">Plan {{ planPartial.id }}: {{ planPartial.name }}</h3>
+    </template>
+    <template #default>
+      <n-form>
+        <n-form-item path="name" label="Name" v-bind="nameProps">
+          <n-input v-model:value="name"></n-input>
+        </n-form-item>
 
-      <template #action>
-        <n-button-group>
-          <n-button type="success" @click="handleCreate">
-            <template #icon>
-              <Icon name="mdi:content-save"/>
-            </template>
-            <span v-text="mode === 'create' ? 'Create' : 'Duplicate'"></span>
-          </n-button>
-          <n-button v-if="mode ==='edit'" type="warning" @click="handleUpdate">
-            <template #icon>
-              <Icon name="mdi:edit"/>
-            </template>
-            <span>Update</span>
-          </n-button>
-          <n-button secondary type="error" @click="handleCancel">
-            <template #icon>
-              <Icon name="mdi:close"/>
-            </template>
-            Cancel
-          </n-button>
-        </n-button-group>
-      </template>
-    </n-card>
+        <n-form-item path="age" label="Age" v-bind="ageProps">
+          <n-input-number v-model:value="age" />
+        </n-form-item>
+
+        <n-form-item path="year" label="Year" v-bind="yearProps">
+          <n-input-number v-model:value="year" />
+        </n-form-item>
+
+        <n-form-item path="lifeExpectancy" label="Life Expectancy" v-bind="lifeExpectancyProps">
+          <n-input-number v-model:value="lifeExpectancy" />
+        </n-form-item>
+
+        <n-divider />
+
+        <n-form-item path="taxRate" label="Tax Rate" v-bind="taxRateProps">
+          <n-slider v-model:value="taxRate" :marks="sliderMarks"></n-slider>
+        </n-form-item>
+
+        <n-form-item path="taxStrategy" label="Tax Strategy" v-bind="taxStrategyProps">
+          <n-select v-model:value="taxStrategy" :options="planForm.taxStrategy.options" />
+        </n-form-item>
+
+        <n-divider />
+
+        <n-form-item path="retirementStrategy" label="Retirement Strategy" v-bind="retirementStrategyProps">
+          <n-select v-model:value="retirementStrategy" :options="planForm.retirementStrategy.options" />
+        </n-form-item>
+
+        <n-form-item
+            path="retirementAge"
+            label="Retirement Age"
+            v-bind="retirementAgeProps"
+            v-if="retirementStrategy === 'age'"
+        >
+          <n-input-number v-model:value="retirementAge" />
+        </n-form-item>
+
+        <n-form-item
+            path="retirementSavingsAmount"
+            label="Retirement Savings Amount"
+            v-bind="retirementSavingsAmountProps"
+            v-if="retirementStrategy === 'target_savings'"
+        >
+          <n-input-number v-model:value="retirementSavingsAmount" />
+        </n-form-item>
+
+        <n-form-item
+            path="retirementWithdrawalRate"
+            label="Retirement Withdrawal Rate"
+            v-bind="retirementWithdrawalRateProps"
+            v-if="retirementStrategy === 'percent_rule'"
+        >
+          <n-input-number v-model:value="retirementWithdrawalRate" />
+        </n-form-item>
+
+        <n-form-item
+            path="retirementIncomeGoal"
+            label="Retirement Income Goal"
+            v-bind="retirementIncomeGoalProps"
+            v-if="retirementStrategy === 'percent_rule'"
+        >
+          <n-input v-model:value="retirementIncomeGoal" />
+        </n-form-item>
+        <n-button :disabled="!meta.valid">
+          Submit
+        </n-button>
+      </n-form>
+
+    </template>
+
+    <template #action>
+      <n-button-group>
+        <n-button secondary round v-if="mode ==='edit'" type="success" @click="handleUpdate">
+          <template #icon>
+            <Icon name="mdi:content-save"/>
+          </template>
+          <span>Save</span>
+        </n-button>
+        <n-button secondary round v-if="mode==='edit'" type="warning" @click="handleCreate">
+          <template #icon>
+            <Icon name="mdi:content-duplicate"/>
+          </template>
+          <span>Duplicate</span>
+        </n-button>
+        <n-button secondary round v-if="mode==='create'" type="success" @click="handleCreate">
+          <template #icon>
+            <Icon name="mdi:content-save"/>
+          </template>
+          <span>Create</span>
+        </n-button>
+        <n-button secondary round type="error" @click="handleCancel">
+          <template #icon>
+            <Icon name="mdi:close"/>
+          </template>
+          Cancel
+        </n-button>
+      </n-button-group>
+    </template>
+  </n-card>
 </template>
 <script lang="ts" setup>
-import {planForm} from "~/forms/planForm";
-import type {PlanTemplate} from "~/models/plan/PlanTemplate";
+import {planForm, planFormSchema} from "~/forms/planForm";
 import type {Plan} from "~/models/plan/Plan";
+import * as yup from "yup"
 
 interface Props {
   planPartial: Partial<Plan>;
@@ -55,8 +120,6 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-
-const planTemplateRef = ref<PlanTemplate>(JSON.parse(JSON.stringify(props.planPartial)))
 
 const taxBrackets = [
   {min: 0, max: 10, label: "Very Low", colorClass: "text-green-500"},
@@ -77,7 +140,7 @@ const sliderMarks = Object.fromEntries(
 const emit = defineEmits(['create', 'update', 'cancel'])
 
 function handleCreate() {
-  emit('create', planTemplateRef.value)
+  emit('create', values)
 
 }
 
@@ -86,8 +149,41 @@ function handleCancel() {
 }
 
 function handleUpdate() {
-  console.log('props', planTemplateRef.value)
-  emit('update', planTemplateRef.value)
+  emit('update', values)
 }
+
+const {defineField, values, errors, handleSubmit, meta } = useForm({
+  validationSchema: planFormSchema,
+  initialValues: props.planPartial
+})
+
+const naiveConfig = (state) => ({
+  props: {
+    validationStatus: state.errors[0] ? 'error' : undefined,
+    feedback: state.errors[0],
+  },
+});
+
+const [name, nameProps] = defineField('name', naiveConfig);
+const [age, ageProps] = defineField("age", naiveConfig);
+const [year, yearProps] = defineField("year", naiveConfig);
+const [lifeExpectancy, lifeExpectancyProps] = defineField("lifeExpectancy", naiveConfig);
+const [taxRate, taxRateProps] = defineField("taxRate", naiveConfig);
+const [taxStrategy, taxStrategyProps] = defineField("taxStrategy", naiveConfig);
+const [retirementStrategy, retirementStrategyProps] = defineField("retirementStrategy", naiveConfig);
+const [retirementAge, retirementAgeProps] = defineField("retirementAge", naiveConfig);
+const [retirementSavingsAmount, retirementSavingsAmountProps] = defineField(
+    "retirementSavingsAmount",
+    naiveConfig
+);
+const [retirementWithdrawalRate, retirementWithdrawalRateProps] = defineField(
+    "retirementWithdrawalRate",
+    naiveConfig
+);
+const [retirementIncomeGoal, retirementIncomeGoalProps] = defineField(
+    "retirementIncomeGoal",
+    naiveConfig
+);
+
 
 </script>
