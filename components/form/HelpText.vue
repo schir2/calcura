f716fa9d2e81c98003e5243a574096fc8ee0996c
Marek Schir <schir2@msn.com>
2024-12-16 19:10:49 -0500

    removed custom fields

--- components/form/HelpText.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-interface Props {
-  helpText: string
-}
-const {helpText = undefined} = defineProps<Props>()
-</script>
-<template>
-  <label class="text-xs block mt-1 italic" v-if="helpText">{{ helpText }}</label>
-</template>
