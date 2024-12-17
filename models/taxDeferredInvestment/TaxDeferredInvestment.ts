ab0d5ba227c4f2ddec2131b70c70c17a0ef8a581
Marek Schir <schir2@msn.com>
2024-12-16 21:50:40 -0500

    added tax deferred investments

--- models/taxDeferredInvestment/TaxDeferredInvestment.ts
+++ models/taxDeferredInvestment/TaxDeferredInvestment.ts
@@ -1,9 +1,12 @@
+import type {Income} from "~/models/income/Income";
+
 export type EmployerContributionStrategy = 'none' | 'percentage_of_contribution' | 'percentage_of_compensation' | 'fixed'
 export type TaxDeferredContributionStrategy = 'none' | 'until_company_match' | 'percentage_of_income' | 'fixed' | 'max'
 
 export interface TaxDeferredInvestment {
     id: number;
     name: string;
+    income?: Income;
 
     growthRate: number;
     initialBalance: number;
@@ -34,6 +37,7 @@ export const taxDeferredInvestmentDefaults: TaxDeferredInvestmentPartial = {
     electiveContributionStrategy: "percentage_of_income",
     electiveContributionPercentage: 0,
     electiveContributionFixedAmount: 0,
+    income: undefined,
 
     employerContributes: false,
     employerContributionStrategy: "percentage_of_contribution",
