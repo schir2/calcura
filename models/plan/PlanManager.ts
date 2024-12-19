11378c9adf32be824c1384098a6b6b8cc9cfd371
Marek Schir <schir2@msn.com>
2024-12-19 01:59:05 -0500

    implemented methods: requestFunds, withdraw, contribute to simplify the processing of investments
    added new states for different types of funds and investment types

--- models/plan/PlanManager.ts
+++ models/plan/PlanManager.ts
@@ -12,6 +12,14 @@ import IraInvestmentManager from "~/models/iraInvestment/IraInvestmentManager";
 import CashReserveManager from "~/models/cashReserve/CashReserveManager";
 import TaxDeferredInvestmentManager from "~/models/taxDeferredInvestment/TaxDeferredInvestmentManager";
 import type {ExpenseType} from "~/models/expense/Expense";
+import {ContributionType} from "~/models/common";
+
+export enum FundType {
+    Taxable = "taxable",
+    Taxed = "taxed",
+
+}
+
 
 export default class PlanManager extends ManagerBase<Plan, PlanState> {
     managers: {
@@ -24,39 +32,124 @@ export default class PlanManager extends ManagerBase<Plan, PlanState> {
         taxDeferredManagers: TaxDeferredInvestmentManager[]
     }
 
-    constructor(config: Plan) {
-        super(config)
+    override setUp(): void {
+
         this.managers = {
-            incomeManagers: config.incomes.map((income) => new IncomeManager(income)),
-            cashReserveManagers: config.cashReserves.map((cashReserve) => new CashReserveManager(cashReserve)),
-            expenseManagers: config.expenses.map((expense) => new ExpenseManager(expense)),
-            debtManagers: config.debts.map((debt) => new DebtManager(debt)),
-            brokerageInvestmentManagers: config.brokerageInvestments.map((brokerageInvestment) => new BrokerageInvestmentManager(brokerageInvestment)),
-            iraInvestmentManagers: config.iraInvestments.map((iraInvestment) => new IraInvestmentManager(iraInvestment)),
-            taxDeferredManagers: config.taxDeferredInvestments.map((taxDeferredInvestment) => new TaxDeferredInvestmentManager(taxDeferredInvestment))
+            incomeManagers: this.config.incomes.map((income) => new IncomeManager(income)),
+            cashReserveManagers: this.config.cashReserves.map((cashReserve) => new CashReserveManager(cashReserve)),
+            expenseManagers: this.config.expenses.map((expense) => new ExpenseManager(expense)),
+            debtManagers: this.config.debts.map((debt) => new DebtManager(debt)),
+            brokerageInvestmentManagers: this.config.brokerageInvestments.map((brokerageInvestment) => new BrokerageInvestmentManager(brokerageInvestment)),
+            iraInvestmentManagers: this.config.iraInvestments.map((iraInvestment) => new IraInvestmentManager(iraInvestment)),
+            taxDeferredManagers: this.config.taxDeferredInvestments.map((taxDeferredInvestment) => new TaxDeferredInvestmentManager(taxDeferredInvestment))
         }
+
     }
 
     protected createInitialState(): PlanState {
-        const grossIncome = this.config.incomes.reduce((agg, income) => agg + income.grossIncome, 0);
+        const grossIncome = this.getGrossIncome()
+        const taxedIncome = grossIncome - this.calculateTaxes(grossIncome)
         return {
             age: this.config.age,
             year: this.config.year,
+
             grossIncome: grossIncome,
             taxableIncome: grossIncome,
-            taxedIncome: 0,
+            taxedIncome: taxedIncome,
+            AGI: 0,
+
+            taxableCapital: grossIncome,
+            taxedCapital: taxedIncome,
+            taxedWithdrawals: 0,
+
+            deductions: 0,
+
+            inflationRate: this.config.inflationRate,
+
             electiveLimit: getTaxDeferredElectiveContributionLimit(this.config.year, this.config.age),
             deferredLimit: getTaxDeferredContributionLimit(this.config.year, this.config.age),
             iraLimit: getIraLimit(this.config.year, this.config.age),
-            inflationRate: this.config.inflationRate,
+
+            savingsTaxDeferredStartOfYear: 0,
+            savingsTaxDeferredEndOfYear: 0,
+
+            savingsTaxExemptStartOfYear: 0,
+            savingsTaxExemptEndOfYear: 0,
+
+            savingsTaxableStartOfYear: 0,
+            savingsTaxableEndOfYear: 0,
+
             savingsStartOfYear: 0,
             savingsEndOfYear: 0,
-            allowNegativeDisposableIncome: this.config.allowNegativeDisposableIncome,
-            processed: false,
+
             retirementIncomeProjected: 0,
             retired: false,
-            growthApplicationStrategy: this.config.growthApplicationStrategy
+
+            processed: false,
         }
+    }
+
+    requestFunds(amount: number, fundType: FundType): number {
+        const currentState = this.getCurrentState()
+        switch (fundType) {
+            case FundType.Taxable:
+                return Math.min(currentState.taxableCapital, amount)
+            case FundType.Taxed:
+                return Math.min(currentState.taxedCapital, amount)
+        }
+    }
+
+
+    contribute(amount: number, contributionType: ContributionType): void {
+        const currentState = this.getCurrentState();
+        switch (contributionType) {
+            case ContributionType.TaxDeferred:
+                currentState.savingsTaxDeferredEndOfYear += amount;
+                break;
+            case ContributionType.TaxExempt:
+                currentState.savingsTaxExemptEndOfYear += amount;
+                break;
+            case ContributionType.Taxable:
+                currentState.savingsTaxableEndOfYear += amount;
+                break;
+        }
+        this.updateCurrentState(currentState);
+    }
+
+    withdraw(amount: number, fundType: FundType): void {
+        const currentState = this.getCurrentState();
+        switch (fundType) {
+            case FundType.Taxable:
+                if (amount > currentState.taxableCapital) {
+                    throw new Error('Insufficient taxable capital for withdrawal');
+                }
+                currentState.taxableCapital -= amount;
+                currentState.taxableIncome -= amount;
+                const agi = this.getAGI(currentState)
+                currentState.taxedIncome = currentState.taxableIncome - this.calculateTaxes(agi)
+                currentState.taxedCapital = currentState.taxedIncome - currentState.taxedWithdrawals
+                this.updateCurrentState(currentState)
+                return
+            case FundType.Taxed:
+                if (amount > currentState.taxedCapital) {
+                    throw new Error('Insufficient taxed capital for tax-exempt contribution');
+                }
+                currentState.taxedCapital -= amount;
+                currentState.taxedWithdrawals += amount;
+                this.updateCurrentState(currentState)
+                return
+            default:
+                throw new Error('Invalid contribution type');
+
+        }
+    }
+
+    getAGI(planState: PlanState): number {
+        return planState.taxableIncome - planState.deductions
+    }
+
+    calculateTaxes(agi: number): number {
+        return agi * (this.config.taxRate / 100)
     }
 
     canRetire(): boolean {
@@ -77,21 +170,48 @@ export default class PlanManager extends ManagerBase<Plan, PlanState> {
     }
 
     getGrossIncome() {
-        return this.managers.incomeManagers.reduce((grossIncome, incomeManager) => incomeManager.getCurrentState().grossIncome, 0)
+        return this.managers.incomeManagers.reduce((grossIncome, incomeManager) => grossIncome + incomeManager.getCurrentState().grossIncome, 0)
+    }
+
+    getInflationRate(): number{
+        return this.config.inflationRate
     }
 
     protected createNextState(previousState: PlanState): PlanState {
         const age = previousState.age + 1
         const year = previousState.year + 1
+        const inflationRate = this.getInflationRate()
         return {
             ...previousState,
             age: age,
             year: year,
-            grossIncome: this.getGrossIncome(),
+
+            grossIncome: 0,
+            taxableIncome: 0,
             taxedIncome: 0,
+            AGI: 0,
+
+            taxableCapital: 0,
+            taxedCapital: 0,
+            taxedWithdrawals: 0,
+
+            deductions: 0,
+
             electiveLimit: getTaxDeferredElectiveContributionLimit(year, age),
             deferredLimit: getTaxDeferredContributionLimit(year, age),
             iraLimit: getIraLimit(year, age),
+
+            inflationRate: inflationRate,
+
+            savingsTaxDeferredStartOfYear: previousState.savingsTaxDeferredEndOfYear,
+            savingsTaxDeferredEndOfYear: 0,
+
+            savingsTaxExemptStartOfYear: previousState.savingsTaxExemptEndOfYear,
+            savingsTaxExemptEndOfYear: 0,
+
+            savingsTaxableStartOfYear: previousState.savingsTaxableEndOfYear,
+            savingsTaxableEndOfYear: 0,
+
             savingsStartOfYear: previousState.savingsEndOfYear,
             savingsEndOfYear: 0,
         }
