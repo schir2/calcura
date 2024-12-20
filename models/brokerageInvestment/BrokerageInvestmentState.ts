84805ebe7385e43f14a724b0e6621ae29fa38299
Marek Schir <schir2@msn.com>
2024-12-19 20:02:14 -0500

    added new properties: contributionLifetime growthLifetime processed

--- models/brokerageInvestment/BrokerageInvestmentState.ts
+++ models/brokerageInvestment/BrokerageInvestmentState.ts
@@ -1,6 +1,9 @@
 export default interface BrokerageInvestmentState {
     contribution: number;
+    contributionLifetime: number;
     growthAmount: number;
+    growthLifetime: number;
     balanceStartOfYear: number;
     balanceEndOfYear?: number;
+    processed: boolean;
 }
