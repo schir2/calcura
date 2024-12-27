14c740dd1c4bea403985a7b4d26f826158eb794a
Marek Schir <schir2@msn.com>
2024-12-26 23:09:18 -0500

    converted cash reserve strategy to enum

--- models/cashReserve/CashReserve.ts
+++ models/cashReserve/CashReserve.ts
@@ -1,4 +1,4 @@
-export type CashReserveStrategy = 'fixed' | 'variable'
+export enum CashReserveStrategy {Fixed = 'Fixed', Variable = 'Variable'}
 
 export interface CashReserve {
     id: number;
@@ -18,7 +18,7 @@ export interface CashReserveTemplate extends CashReserve {
 export const cashReserveDefaults: CashReservePartial = {
     name: 'Blank Reserve',
     initialAmount: 0,
-    cashReserveStrategy: 'fixed',
+    cashReserveStrategy: CashReserveStrategy.Fixed,
     reserveAmount: 0,
     reserveMonths: 0,
 }
