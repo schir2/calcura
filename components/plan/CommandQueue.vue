b931f41f4e440deee24bc2156f6794727327c16a
Marek Schir <schir2@msn.com>
2025-01-13 21:21:58 -0500

    Add CommandQueue component to display a list of commands
    This component renders a list of commands using the provided `commands` prop. It leverages a template and TypeScript setup, ensuring proper typing for the command objects.

---
+++ components/plan/CommandQueue.vue
@@ -0,0 +1,19 @@
+<template>
+  <n-list>
+    <n-list-item v-for="command in orderedCommands" :key="command.getName()">
+      {{command.getName()}}
+    </n-list-item>
+  </n-list>
+</template>
+<script setup lang="ts">
+import type Command from "~/models/common/Command";
+
+interface Props {
+  commands: Command[]
+}
+
+const props = defineProps<Props>()
+const orderedCommands = toRef(props, 'commands')
+
+
+</script>
