17ca79b97de1cdbd1a60969a9a5968747eaec9d9
Marek Schir <schir2@msn.com>
2025-01-20 23:44:08 -0500

    Add `shortfall` property to ExpenseState model
    The `shortfall` property has been introduced to the ExpenseState model to represent the difference between the requested and paid amounts. This update will provide more clarity when handling expense calculations and enhance the model's functionality.

--- models/expense/ExpenseState.ts
+++ models/expense/ExpenseState.ts
@@ -2,6 +2,7 @@ export default interface ExpenseState {
     baseAmount: number;
     amountRequested: number;
     amountPaid: number;
+    shortfall: number;
     growthAmount: number;
     processed: boolean;
 }
