0dcdf3864b1f4f800ed307f330160cf33eabe8de
Marek Schir <schir2@msn.com>
2025-01-20 23:44:46 -0500

    Add Table.vue component to render plan states table
    Introduces a new table component displaying various financial metrics for plan states. The table is dynamically populated using the passed `planStates` prop, with humanized number formatting for better readability.

---
+++ components/plan/Table.vue
@@ -0,0 +1,48 @@
+<template>
+    <n-table size="small">
+      <thead>
+      <tr>
+        <th>Age</th>
+        <th>Gross Income</th>
+        <th>Taxable Income</th>
+        <th>Taxed Income</th>
+        <th>Taxed Capital</th>
+        <th>Taxable Capital</th>
+        <th>Taxed Withdrawals</th>
+        <th>Total</th>
+        <th>Paid</th>
+        <th>Shortfall</th>
+        <th>Tax Deferred</th>
+        <th>Tax Exempt</th>
+        <th>Taxable</th>
+        <th>Gross</th>
+
+      </tr>
+
+      </thead>
+      <tbody>
+      <tr class="space-y-3" v-for="state in planStates">
+        <td>{{ state.age }}</td>
+        <td>${{ $humanize.intComma(state.grossIncome) }}</td>
+        <td>${{ $humanize.intComma(state.taxableIncome) }}</td>
+        <td>${{ $humanize.intComma(state.taxedIncome) }}</td>
+        <td>${{ $humanize.intComma(state.taxedCapital) }}</td>
+        <td>${{ $humanize.intComma(state.taxableCapital) }}</td>
+        <td>${{ $humanize.intComma(state.taxedWithdrawals) }}</td>
+        <td>${{ $humanize.intComma(state.expensesTotal) }}</td>
+        <td>${{ $humanize.intComma(state.expensesPaid) }}</td>
+        <td>${{ $humanize.intComma(state.expensesShortfall) }}</td>
+        <td>${{ $humanize.intComma(state.savingsTaxDeferredEndOfYear) }}</td>
+        <td>${{ $humanize.intComma(state.savingsTaxExemptEndOfYear) }}</td>
+        <td>${{ $humanize.intComma(state.savingsTaxableEndOfYear) }}</td>
+        <td>${{ $humanize.intComma(state.savingsEndOfYear) }}</td>
+      </tr>
+      </tbody>
+    </n-table>
+</template>
+<script setup lang="ts">
+import type {PlanState} from "~/models/plan/PlanState";
+
+interface Props {planStates: PlanState[]}
+const props = defineProps<Props>()
+</script>
