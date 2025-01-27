e38c905d7d1d1a3f224b82b76db723898805e9dd
Marek Schir <schir2@msn.com>
2025-01-27 15:47:31 -0500

    added draggable pointer as well as draggable icon

--- components/plan/CommandQueue.vue
+++ components/plan/CommandQueue.vue
@@ -1,16 +1,19 @@
 <template>
 
-      <VueDraggableNext v-if="list"
-                        class="dragArea list-group w-full" :list="list" @change="onChange">
-        <div
-            class="list-group-item bg-skin-surface m-1 p-3 rounded-md text-center"
-            v-for="element in list"
-            :key="element.name"
-        >
-          {{ element.name }}
-          <n-tag>{{ element.label }}</n-tag>
-        </div>
-      </VueDraggableNext>
+  <VueDraggableNext v-if="list"
+                    class="dragArea list-group w-full" :list="list" @change="onChange">
+    <p
+        class="list-group-item bg-skin-surface m-1 p-3 rounded-md text-center cursor-pointer flex justify-between items-center"
+        v-for="element in list"
+        :key="element.name"
+    >
+          <span class="flex items-center gap-3">
+          <Icon class="text-2xl" name="mdi:drag"/>
+      {{ element.name }}
+          </span>
+      <n-tag>{{ element.label }}</n-tag>
+    </p>
+  </VueDraggableNext>
 
 </template>
 <script setup lang="ts">
