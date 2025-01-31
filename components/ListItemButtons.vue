c38e2b47273fcba46a4991f0d6f96d0164c99492
Marek Schir <schir2@msn.com>
2025-01-31 00:13:03 -0500

    added confirmations to the remove and delete actions

--- components/ListItemButtons.vue
+++ components/ListItemButtons.vue
@@ -5,21 +5,74 @@
         <Icon name="mdi:edit"/>
       </template>
     </n-button>
-    <n-button tertiary round type="error" @click="$emit('remove')">
+    <n-popconfirm v-model:show="showRemovePopConfirm">
       <template #icon>
-        <Icon name="mdi:remove"/>
+        <Icon class="text-6xl text-skin-error" name="mdi:remove"/>
       </template>
-    </n-button>
-    <n-button secondary round type="error" @click="$emit('delete')">
+      <template #action>
+        <n-button tertiary round @click="showRemovePopConfirm=false">Cancel</n-button>
+        <n-button tertiary type="error" round @click=handleRemove()>Remove</n-button>
+      </template>
+      <template #trigger>
+        <n-button tertiary round type="error">
+          <template #icon>
+            <Icon name="mdi:remove"/>
+          </template>
+        </n-button>
+      </template>
+      <div class="max-w-md px-3 pe-3">
+        <h2 class="text-xl my-3 text-skin-error font-semibold">Remove from Plan</h2>
+        <p>Are you sure you want to remove this item from the plan?</p>
+        <p class="text-skin-info text-xs mb-2">This will only remove this from the plan, it will sill exist on your account.</p>
+      </div>
+    </n-popconfirm>
+    <n-popconfirm v-model:show="showDeletePopConfirm">
       <template #icon>
-        <Icon name="mdi:delete"/>
+        <Icon class="text-6xl text-skin-error" name="mdi:delete"/>
       </template>
-    </n-button>
+      <template #action>
+        <n-button tertiary round @click="showDeletePopConfirm=false">Cancel</n-button>
+        <n-button tertiary type="error" round @click=handleDelete()>
+          <template #icon>
+            <Icon name="mdi:delete"/>
+          </template>
+          Delete
+        </n-button>
+      </template>
+      <template #trigger>
+        <n-button secondary round type="error">
+          <template #icon>
+            <Icon name="mdi:delete"/>
+          </template>
+        </n-button>
+      </template>
+      <div class="max-w-md px-3 pe-3">
+        <h2 class="text-xl my-3 text-skin-error font-semibold">Delete Item</h2>
+        <p>Are you sure you want to delete this item?</p>
+        <p class="text-skin-info text-xs mb-2">This will permanently remove this from this plan and all other plans as well as your account.</p>
+      </div>
+
+    </n-popconfirm>
   </n-button-group>
 </template>
 <script setup lang="ts">
 interface Props {
   size: "tiny" | "small" | "medium" | "large"
 }
+
 const props = defineProps<Props>()
+const showRemovePopConfirm = ref(false)
+const showDeletePopConfirm = ref(false)
+
+const emit = defineEmits(['remove', 'delete', 'edit'])
+
+function handleRemove() {
+  showRemovePopConfirm.value = false
+  emit('remove')
+}
+
+function handleDelete() {
+  showDeletePopConfirm.value = false
+  emit('delete')
+}
 </script>
