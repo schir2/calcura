8f3860a7bdc0878f67934187223e80c95bd48cbb
Marek Schir <schir2@msn.com>
2025-02-28 14:48:29 -0500

    implemented handleCreatePlanModel

--- pages/plans/[id].vue
+++ pages/plans/[id].vue
@@ -9,64 +9,65 @@
       </n-modal>
       <PlanDetailCard :plan="plan" @update="handleUpdate"></PlanDetailCard>
       <nav>
-...
+        ...
       </nav>
       <section class="grid gap-2">
-        <ChildCreateButtonList @create-model="handleCreateModel($event)" />
-          <DebtListItem v-for="debt in plan.debts" :key="debt.id" :debt="debt"
-                    @update="handleUpdateDebt"
-                    @delete="handleDeleteDebt"
-                    @remove="handleRemoveDebt" />
-          <ExpenseListItem v-for="expense in plan.expenses" :key="expense.id" :expense="expense"
-                       @update="handleUpdateExpense"
-                       @delete="handleDeleteExpense"
-                       @remove="handleRemoveExpense"
-          />
+        <ChildCreateButtonList @create-model="handleCreatePlanModel($event)"/>
+        <DebtListItem v-for="debt in plan.debts" :key="debt.id" :debt="debt"
+                      @update="handleUpdateDebt"
+                      @delete="handleDeleteDebt"
+                      @remove="handleRemoveDebt"/>
 
-          <IncomeListItem v-for="income in plan.incomes" :key="income.id" :income="income"
-                      @create="handleCreateIncome"
-                      @update="handleUpdateIncome"
-                      @delete="handleDeleteIncome"
-                      @remove="handleRemoveIncome"
-          />
-          <CashReserveListItem v-for="cashReserve in plan.cashReserves" :key="cashReserve.id" :cashReserve="cashReserve"
-                           @update="handleUpdateCashReserve"
-                           @delete="handleDeleteCashReserve"
-                           @remove="handleRemoveCashReserve"
-          />
+        <ExpenseListItem v-for="expense in plan.expenses" :key="expense.id" :expense="expense"
+                         @update="handleUpdateExpense"
+                         @delete="handleDeleteExpense"
+                         @remove="handleRemoveExpense"
+        />
 
-
-          <BrokerageListItem v-for="brokerage in plan.brokerages" :key="brokerage.id" :brokerage="brokerage"
-                                   @update="handleUpdateBrokerage"
-                                   @delete="handleDeleteBrokerage"
-                                   @remove="handleRemoveBrokerage"
-          />
+        <IncomeListItem v-for="income in plan.incomes" :key="income.id" :income="income"
+                        @create="handleCreateIncome"
+                        @update="handleUpdateIncome"
+                        @delete="handleDeleteIncome"
+                        @remove="handleRemoveIncome"
+        />
+        <CashReserveListItem v-for="cashReserve in plan.cashReserves" :key="cashReserve.id" :cashReserve="cashReserve"
+                             @update="handleUpdateCashReserve"
+                             @delete="handleDeleteCashReserve"
+                             @remove="handleRemoveCashReserve"
+        />
 
 
-          <IraListItem v-for="ira in plan.iras" :key="ira.id" :ira="ira"
-                             @update="handleUpdateIra"
-                             @delete="handleDeleteIra"
-                             @remove="handleRemoveIra"
-          />
+        <BrokerageListItem v-for="brokerage in plan.brokerages" :key="brokerage.id" :brokerage="brokerage"
+                           @update="handleUpdateBrokerage"
+                           @delete="handleDeleteBrokerage"
+                           @remove="handleRemoveBrokerage"
+        />
 
 
-          <RothIraListItem v-for="rothIra in plan.rothIras" :key="rothIra.id" :rothIra="rothIra"
-                                 @update="handleUpdateRothIra"
-                                 @delete="handleDeleteRothIra"
-                                 @remove="handleRemoveRothIra"
-          />
+        <IraListItem v-for="ira in plan.iras" :key="ira.id" :ira="ira"
+                     @update="handleUpdateIra"
+                     @delete="handleDeleteIra"
+                     @remove="handleRemoveIra"
+        />
 
 
-          <TaxDeferredListItem
-              v-for="taxDeferred in plan.taxDeferreds"
-              :key="taxDeferred.id"
-              :taxDeferred="taxDeferred"
-              :incomes="plan.incomes"
-              @update="handleUpdateTaxDeferred"
-              @delete="handleDeleteTaxDeferred"
-              @remove="handleRemoveTaxDeferred"
-          />
-        </section>
+        <RothIraListItem v-for="rothIra in plan.rothIras" :key="rothIra.id" :rothIra="rothIra"
+                         @update="handleUpdateRothIra"
+                         @delete="handleDeleteRothIra"
+                         @remove="handleRemoveRothIra"
+        />
+
+
+        <TaxDeferredListItem
+            v-for="taxDeferred in plan.taxDeferreds"
+            :key="taxDeferred.id"
+            :taxDeferred="taxDeferred"
+            :incomes="plan.incomes"
+            @update="handleUpdateTaxDeferred"
+            @delete="handleDeleteTaxDeferred"
+            @remove="handleRemoveTaxDeferred"
+        />
+      </section>
     </div>
     <div>
       <PlanSimulation
@@ -85,17 +86,14 @@ import type {Expense, ExpensePartial} from "~/types/Expense"
 import type {Plan} from "~/types/Plan";
 import type {CashReserve} from "~/types/CashReserve";
 import type {Ira, IraPartial} from "~/types/Ira";
-import type {
-  TaxDeferred,
-  TaxDeferredPartial
-} from "~/types/TaxDeferred";
+import type {TaxDeferred, TaxDeferredPartial} from "~/types/TaxDeferred";
 import type {Income, IncomePartial} from "~/types/Income";
 import type {Brokerage, BrokeragePartial} from "~/types/Brokerage";
 import type {RothIra, RothIraPartial} from "~/types/RothIra";
-import {type Command} from "~/types/Command";
 import ChildCreateButtonList from "~/components/plan/ChildCreateButtonList.vue";
 import type {ModelName} from "~/types/ModelName";
 import type {CommandSequence} from "~/types/CommandSequence";
+import {toKebabCaseKey} from "~/utils";
 
 const planService = usePlanService()
 const debtService = useDebtService()
@@ -292,17 +290,13 @@ async function handleRemoveTaxDeferred(taxDeferredPartial: TaxDeferredPartial) {
   await loadPlan();
 }
 
-const repo = useRepo()
-async function handleCreateModel(payload: {model: ModelName, data: any}){
-  if (!repo[payload.model]) {
-    throw new Error(`Model ${payload.model} does not exist in the repository.`);
-  }
-  const relatedModel = await repo[payload.model].create(payload.data)
-  console.log(toSnakeCase(payload.model))
-  await repo.plan.addRelatedModel( planId, `${toSnakeCase(payload.model)}s`, relatedModel.id,)
+async function handleCreatePlanModel(payload: { model: ModelName, data: any }) {
+  const p = await useApi<Expense>(`plans/${planId}/${toKebabCaseKey(payload.model)}s`).create(payload.data)
   await refreshPlan();
 
 }
+
+const repo = useRepo()
 
 async function handleCommandSequenceUpdate(commandSequence: CommandSequence) {
   await repo.commandSequence.update(commandSequence.id, commandSequence)
