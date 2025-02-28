4981735140d3c1da1dcc6a2d2daec314411c6bab
Marek Schir <schir2@msn.com>
2025-02-27 23:13:44 -0500

    removed transitions

--- components/plan/Form.vue
+++ components/plan/Form.vue
@@ -1,25 +1,3 @@
-<template>
-  <n-card role="dialog" class="max-w-6xl" :bordered="true">
-    <template #header>
-      <h3 class="text-2xl mb-2">Plan{{ modelRef.name }}</h3>
-      <PlanFormSteps v-model="currentStep" @update=handleStepChange />
-    </template>
-    <template #default>
-      <n-form ref="formRef" :model="modelRef" :rules="rules">
-        <transition mode="out-in" :name="transitionName">
-          <PlanFormProfile v-if="currentStep===1" v-model="modelRef"/>
-          <PlanFormSettings v-else-if="currentStep===2" v-model="modelRef"/>
-          <PlanFormGoals v-else-if="currentStep===3" v-model="modelRef"/>
-        </transition>
-      </n-form>
-
-    </template>
-
-    <template #action>
-      <FormActionButtons :mode="mode" @update="handleUpdate" @create="handleCreate" @cancel="handleCancel"/>
-    </template>
-  </n-card>
-</template>
 <script lang="ts" setup>
 import {type Plan} from "~/types/Plan";
 import {usePlanValidator} from "~/composables/validators/usePlanValidator";
@@ -38,39 +16,27 @@ const {formRef, modelRef, rules, handleCreate, handleUpdate, handleCancel} =
 const currentStep = ref<number>(1);
 const previousStep = ref<number>(1);
 
-function handleStepChange(payload: {value: number, oldValue: number}){
+function handleStepChange(payload: { value: number, oldValue: number }) {
 }
-
-const transitionName = computed(() => {
-  return currentStep.value > previousStep.value ? "slide-left" : "slide-right";
-});
 
 </script>
+<template>
+  <n-card role="dialog" class="max-w-6xl" :bordered="true">
+    <template #header>
+      <h3 class="text-2xl mb-2">Plan{{ modelRef.name }}</h3>
+      <PlanFormSteps v-model="currentStep" @update=handleStepChange/>
+    </template>
+    <template #default>
+      <n-form ref="formRef" :model="modelRef" :rules="rules">
+        <PlanFormProfile v-if="currentStep===1" v-model="modelRef"/>
+        <PlanFormSettings v-else-if="currentStep===2" v-model="modelRef"/>
+        <PlanFormGoals v-else-if="currentStep===3" v-model="modelRef"/>
+      </n-form>
 
-<style scoped>
-/* Define transitions */
-.slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
-}
+    </template>
 
-.slide-left-enter-from {
-  transform: translateX(100%);
-  opacity: 0;
-}
-.slide-left-leave-to {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-
-.slide-right-enter-from {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-.slide-right-leave-to {
-  transform: translateX(100%);
-  opacity: 0;
-}
-</style>
+    <template #action>
+      <FormActionButtons :mode="mode" @update="handleUpdate" @create="handleCreate" @cancel="handleCancel"/>
+    </template>
+  </n-card>
+</template>
