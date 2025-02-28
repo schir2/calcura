668ca8c03032240aa2a04e68942e0086bc598dde
Marek Schir <schir2@msn.com>
2025-02-27 23:20:18 -0500

    swapped template with script added defaults

--- components/brokerageInvestment/Form.vue
+++ components/brokerageInvestment/Form.vue
@@ -1,71 +1,20 @@
-<template>
-  <n-card role="dialog" class="max-w-4xl" :bordered="true">
-    <template #header>
-      <h3 class="text-2xl">Brokerage Investment: {{ modelRef.name }}</h3>
-    </template>
-
-    <template #default>
-      <n-form ref="formRef" :model="modelRef" :rules="rules">
-        <section class="grid grid-cols-3 gap-3">
-          <n-form-item label="Name" path="name">
-            <n-input v-model:value="modelRef.name" placeholder="Enter investment name"/>
-          </n-form-item>
-
-          <n-form-item label="Initial Balance" path="initialBalance">
-            <n-input-number class="w-full" v-model:value="modelRef.initialBalance" placeholder="Enter initial balance"/>
-          </n-form-item>
-
-          <n-form-item label="Growth Rate (%)" path="growthRate">
-            <n-input-number class="w-full" v-model:value="modelRef.growthRate" placeholder="Enter growth rate"/>
-          </n-form-item>
-        </section>
-        <n-form-item label="Contribution Strategy" path="contributionStrategy">
-          <div class="grid grid-cols-3 gap-3 w-full">
-            <CommonRadioCard v-model="modelRef.contributionStrategy" :value="BrokerageContributionStrategy.Fixed"
-                             title="Fixed">
-              <n-form-item label="Fixed Contribution Amount" path="contributionFixedAmount">
-                <n-input-number class="w-full" v-model:value="modelRef.contributionFixedAmount"
-                                placeholder="Enter fixed amount"/>
-              </n-form-item>
-            </CommonRadioCard>
-            <CommonRadioCard v-model="modelRef.contributionStrategy"
-                             :value="BrokerageContributionStrategy.PercentageOfIncome" title="Percentage of Income">
-              <n-form-item label="Contribution Percentage (%)" path="contributionPercentage">
-                <n-input-number class="w-full" v-model:value="modelRef.contributionPercentage"
-                                placeholder="Enter percentage"/>
-              </n-form-item>
-            </CommonRadioCard>
-            <CommonRadioCard v-model="modelRef.contributionStrategy" :value="BrokerageContributionStrategy.Max"
-                             title="Max Out"/>
-          </div>
-        </n-form-item>
-
-      </n-form>
-    </template>
-
-    <template #action>
-      <FormActionButtons :mode="mode" @update="handleUpdate" @create="handleCreate" @cancel="handleCancel"/>
-    </template>
-  </n-card>
-</template>
-
 <script lang="ts" setup>
 import {
   BrokerageContributionStrategy,
-  type BrokerageInvestment,
+  type BrokerageInvestment, brokerageInvestmentDefaults,
   type BrokerageInvestmentPartial
 } from "~/types/BrokerageInvestment";
 import type {FormInst} from "naive-ui";
 import {useBrokerageInvestmentValidator} from "~/composables/validators/useBrokerageInvestmentValidator";
 
 interface Props {
-  initialValues: Partial<BrokerageInvestment>;
+  initialValues?: Partial<BrokerageInvestment>;
   mode: 'create' | 'edit' | 'view'
 }
 
-const props = defineProps<Props>();
+const {initialValues = brokerageInvestmentDefaults, mode} = defineProps<Props>();
 const emit = defineEmits(["update", "cancel", "create"]);
-const modelRef = ref<BrokerageInvestmentPartial>(props.initialValues)
+const modelRef = ref<BrokerageInvestmentPartial>(initialValues)
 const formRef = ref<FormInst | null>(null);
 const {handleCreate, handleUpdate, handleCancel} = useCrudForm(emit, formRef, modelRef)
 const {rules} = useBrokerageInvestmentValidator(modelRef)
@@ -126,3 +75,53 @@ export type BrokerageInvestmentProjection = {
 //   return result;
 // });
 </script>
+<template>
+  <n-card role="dialog" class="max-w-4xl" :bordered="true">
+    <template #header>
+      <h3 class="text-2xl">Brokerage Investment: {{ modelRef.name }}</h3>
+    </template>
+
+    <template #default>
+      <n-form ref="formRef" :model="modelRef" :rules="rules">
+        <section class="grid grid-cols-3 gap-3">
+          <n-form-item label="Name" path="name">
+            <n-input v-model:value="modelRef.name" placeholder="Enter investment name"/>
+          </n-form-item>
+
+          <n-form-item label="Initial Balance" path="initialBalance">
+            <n-input-number class="w-full" v-model:value="modelRef.initialBalance" placeholder="Enter initial balance"/>
+          </n-form-item>
+
+          <n-form-item label="Growth Rate (%)" path="growthRate">
+            <n-input-number class="w-full" v-model:value="modelRef.growthRate" placeholder="Enter growth rate"/>
+          </n-form-item>
+        </section>
+        <n-form-item label="Contribution Strategy" path="contributionStrategy">
+          <div class="grid grid-cols-3 gap-3 w-full">
+            <CommonRadioCard v-model="modelRef.contributionStrategy" :value="BrokerageContributionStrategy.Fixed"
+                             title="Fixed">
+              <n-form-item label="Fixed Contribution Amount" path="contributionFixedAmount">
+                <n-input-number class="w-full" v-model:value="modelRef.contributionFixedAmount"
+                                placeholder="Enter fixed amount"/>
+              </n-form-item>
+            </CommonRadioCard>
+            <CommonRadioCard v-model="modelRef.contributionStrategy"
+                             :value="BrokerageContributionStrategy.PercentageOfIncome" title="Percentage of Income">
+              <n-form-item label="Contribution Percentage (%)" path="contributionPercentage">
+                <n-input-number class="w-full" v-model:value="modelRef.contributionPercentage"
+                                placeholder="Enter percentage"/>
+              </n-form-item>
+            </CommonRadioCard>
+            <CommonRadioCard v-model="modelRef.contributionStrategy" :value="BrokerageContributionStrategy.Max"
+                             title="Max Out"/>
+          </div>
+        </n-form-item>
+
+      </n-form>
+    </template>
+
+    <template #action>
+      <FormActionButtons :mode="mode" @update="handleUpdate" @create="handleCreate" @cancel="handleCancel"/>
+    </template>
+  </n-card>
+</template>
