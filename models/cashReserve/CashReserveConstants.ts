d9259e77602f37f90e4d07ecca4cbf9bfcae9696
Marek Schir <schir2@msn.com>
2025-02-28 00:31:32 -0500

    fixed some odd typing

--- models/cashReserve/CashReserveConstants.ts
+++ models/cashReserve/CashReserveConstants.ts
@@ -1,4 +1,3 @@
-import type {CashReserveStrategy} from "~/types/CashReserve";
 import {CashReserveStrategy} from "~/types/CashReserve";
 
 export const DEFAULT_CASH_MAINTENANCE_NAME = 'Cash Reserve 6 months of Expenses';
@@ -8,5 +7,5 @@ export const DEFAULT_CASH_MAINTENANCE_RESERVE_MONTHS = 6;
 
 export const MIN_RESERVE_AMOUNT = 0;
 export const MAX_RESERVE_AMOUNT = 100_000_000;
-export const MIN_RESERVE_MONTHS = 1;
+export const MIN_RESERVE_MONTHS = 0;
 export const MAX_RESERVE_MONTHS = 60;
