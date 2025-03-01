a7bbd70344e785dee85b0f78412ded60b205b480
Marek Schir <schir2@msn.com>
2025-03-01 11:21:52 -0500

    removed fancy bg for now and removed title

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,16 @@
       <n-loading-bar-provider>
         <n-config-provider :theme="darkTheme">
           <n-message-provider>
-            <n-space vertical :naive-scrollbar="false">
-              <LayoutTheNavbar/>
-              <n-layout has-sider class="min-h-nav-offset" :naive-scrollbar="false">
-                <LayoutTheLeftSidebar/>
-                <n-layout class="mr-2" :native-scrollbar="false">
-                  <effects-aurora-background class="p-4">
-                    <h1 class="text-4xl mb-4">{{ title }}</h1>
+              <n-space vertical>
+                <LayoutTheNavbar/>
+                <n-layout has-sider class="min-h-nav-offset" :naive-scrollbar="false">
+                  <LayoutTheLeftSidebar/>
+                  <n-layout class="mr-2" :native-scrollbar="false">
+                    <!--                    <h1 class="text-4xl mb-4">{{ title }}</h1>-->
                     <slot/>
-                  </effects-aurora-background>
+                  </n-layout>
                 </n-layout>
-              </n-layout>
-            </n-space>
+              </n-space>
           </n-message-provider>
         </n-config-provider>
       </n-loading-bar-provider>
