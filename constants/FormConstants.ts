c725ebe15c0e63f486f13c5871fd77631cd964b3
Marek Schir <schir2@msn.com>
2025-03-01 13:10:19 -0500

    changed default form width

--- constants/FormConstants.ts
+++ constants/FormConstants.ts
@@ -1,3 +1,3 @@
 export const FORM_LABEL_PLACEMENT: 'left' | 'top' = 'left';
 export const FORM_LABEL_ALIGN: 'left' | 'right' = 'left';
-export const FORM_MODAL_WIDTH_CLASS: string = 'max-w-xl'
+export const FORM_MODAL_WIDTH_CLASS: string = 'max-w-lg'
