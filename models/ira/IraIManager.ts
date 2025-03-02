c64c6d288058301210c2308002c3f920af30cf41
Marek Schir <schir2@msn.com>
2025-03-01 22:42:38 -0500

    added a new function for calculating contribution to be more testable

--- models/ira/IraIManager.ts
+++ models/ira/IraIManager.ts
@@ -1,4 +1,4 @@
-import {IraContributionStrategy, type Ira} from '~/types/Ira';
+import {type Ira, IraContributionStrategy} from '~/types/Ira';
 import {assertDefined, calculateGrowthAmount} from "~/utils";
 import type IraState from "~/types/IraState";
 import BaseManager from "~/models/common/BaseManager";
@@ -6,6 +6,7 @@ import type {IncomeManager} from "~/models/income/IncomeManager";
 import {FundType} from "~/models/plan/PlanManager";
 import {ContributionType} from "~/models/common";
 import eventBus from "~/services/eventBus";
+import {IRA_CONTRIBUTION_LIMIT_2024} from "~/constants/IraIConstants";
 
 export class IraIManager extends BaseManager<Ira, IraState> {
 
@@ -31,24 +32,11 @@ export class IraIManager extends BaseManager<Ira, IraState> {
     }
 
     calculateContribution(): number {
-        let contribution = 0
-        switch (this.config.contributionStrategy) {
-            case IraContributionStrategy.Fixed:
-                contribution = this.config.contributionFixedAmount
-                break
-            case IraContributionStrategy.PercentageOfIncome:
-                if (this.incomeManager === undefined) {
-
-                    eventBus.emit('warning', {scope: 'iraManager:missingIncomeManager', message: 'Cannot perform percentage of income without a lined income manager'})
-                    return 0
-                }
-                contribution = this.incomeManager.getCurrentState().grossIncome * this.config.contributionPercentage / 100
-                break
-            case IraContributionStrategy.Max:
-                contribution = Infinity
-                break
-        }
-        return Math.min(contribution, this.orchestrator.getCurrentState().iraLimit)
+        return calculateIraContribution(
+            this.config,
+            this.incomeManager?.getCurrentState().grossIncome,
+            this.orchestrator.getCurrentState().iraLimit
+        );
     }
 
     createNextState(previousState: IraState): IraState {
@@ -90,4 +78,29 @@ export class IraIManager extends BaseManager<Ira, IraState> {
             }
         )
     }
+}
+
+
+export function calculateIraContribution(iraConfig: Ira, incomeAmount?: number, iraLimit: number = IRA_CONTRIBUTION_LIMIT_2024): number {
+    let contribution = 0
+    switch (iraConfig.contributionStrategy) {
+        case IraContributionStrategy.Fixed:
+            contribution = iraConfig.contributionFixedAmount
+            break
+        case IraContributionStrategy.PercentageOfIncome:
+            if (incomeAmount === undefined) {
+
+                eventBus.emit('warning', {
+                    scope: 'iraManager:missingIncomeManager',
+                    message: 'Cannot perform percentage of income without a lined income manager'
+                })
+                return 0
+            }
+            contribution = incomeAmount * iraConfig.contributionPercentage / 100
+            break
+        case IraContributionStrategy.Max:
+            contribution = iraLimit
+            break
+    }
+    return Math.min(contribution, iraLimit)
 }
