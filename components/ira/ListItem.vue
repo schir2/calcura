33cd87c617c26d88ceb8074f9bd6e5cb27af6d58
Marek Schir <schir2@msn.com>
2025-03-01 22:45:14 -0500

    implemented command list item for debt and ira

--- components/ira/ListItem.vue
+++ components/ira/ListItem.vue
@@ -1,29 +1,8 @@
-<template>
-  <n-modal v-model:show="showModal">
-    <IraForm :initialValues="ira" mode="edit"
-                 @delete="handleDelete"
-                 @create="handleCreate"
-                 @update="handleUpdate"
-                 @cancel="handleClose"
-    />
-  </n-modal>
-    <n-card size="small">
-      <template #header>
-        <span>{{ ira.name }}</span>
-      </template>
-      <template #default>
-        <ul class="grid grid-cols-5">
-        </ul>
-      </template>
-      <template #header-extra>
-        <ListItemButtons size="small" @edit="handleEdit" @remove="handleRemove" @delete="handleDelete"/>
-      </template>
-    </n-card>
-
-</template>
 <script setup lang="ts">
 
 import type {Ira} from "~/types/Ira";
+import {ModelName} from "~/types/ModelName";
+import {calculateIraContribution} from "~/models/ira/IraIManager";
 
 interface Props {
   ira: Ira
@@ -63,3 +42,39 @@ function handleEdit() {
   showModal.value = true;
 }
 </script>
+<template>
+  <n-modal v-model:show="showModal">
+    <IraForm :initialValues="ira" mode="edit"
+                 @delete="handleDelete"
+                 @create="handleCreate"
+                 @update="handleUpdate"
+                 @cancel="handleClose"
+    />
+  </n-modal>
+  <command-list-item
+      @edit="handleEdit" @remove="handleRemove" @delete="handleDelete"
+      :title="ira.name"
+      :modelName="ModelName.Ira"
+      :tags="[
+          {label: ira.contributionStrategy, },
+          {label: `Growth ${ira.growthRate}%`, iconName: 'growthRate', hide: ira.growthRate === 0},
+      ]"
+  >
+    <template #summary>
+      -${{ $humanize.intComma(calculateIraContribution(ira, ira.income?.grossIncome, IRA_CONTRIBUTION_LIMIT_2024 )) }}/year
+    </template>
+  </command-list-item>
+    <n-card size="small">
+      <template #header>
+        <span>{{ ira.name }}</span>
+      </template>
+      <template #default>
+        <ul class="grid grid-cols-5">
+        </ul>
+      </template>
+      <template #header-extra>
+        <ListItemButtons size="small" @edit="handleEdit" @remove="handleRemove" @delete="handleDelete"/>
+      </template>
+    </n-card>
+
+</template>
